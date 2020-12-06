371b83b72f67ec8a23cc0ce06fe6389af90ce4de
gorar <goran.dakov@gmail.com>
2020-12-06 10:55:52 +0000

    dbl add swap on exp

--- rtl_test/v_fpu/dbl.cpp
+++ rtl_test/v_fpu/dbl.cpp
@@ -4,6 +4,25 @@
 void req::opADDd (int iA,int iB, int iRes, int rmode) {
   unsigned expA=((A[iA]>>52)&0x7ff)|((Ax[iA]&2)<<10);
   unsigned expB=((B[iB]>>52)&0x7ff)|((Bx[iB]&2)<<10);
+
+  unsigned long mantA=(A[iA]&0xfffffffffffff)|0x80000000000000;
+  unsigned long mantB=(B[iB]&0xfffffffffffff)|0x80000000000000;
+
+  unsigned sigA=A[iA]>>63;
+  unsigned sigB=B[iB]>>63;
+
+  if (expB>expA) {
+      unsigned long swp;
+      swp=expA;
+      expA=expB;
+      expB=swp;
+      swp=mantA;
+      mantA=mantB;
+      mantB=swp;
+      swp=sigA;
+      sigA=sigB;
+      sigB=swp;
+  }
 }
 
 void req::opSUBd (int iA,int iB, int iRes, int rmode) {
