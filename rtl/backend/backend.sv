fad8d058ab1ebe4db78c03a0bc9ee9c1536f556b
G Dakov <goran.dakov@gmail.com>
2021-01-31 12:30:04 -0500

    began adding fun_fpsu_BOTH to backend

--- rtl/backend/backend.sv
+++ rtl/backend/backend.sv
@@ -5512,6 +5512,40 @@ module backend(
   fxFCADD5_raise_s
   );
 
+  fun_fpsu_BOTH fpsu_mod(
+  clk,
+  rst,
+  u1_A0,u1_B0,u1_A1,u1_B1,u1_en,u1_op,
+  u1_fufwd_A,u1_fuufwd_A,u1_fufwd_B,u1_fuufwd_B,
+  u1_ret,u1_ret_en,
+  u2_A0,u2_B0,u2_A1,u2_B1,u2_en,u2_op,
+  u2_fufwd_A,u2_fuufwd_A,u2_fufwd_B,u2_fuufwd_B,
+  u2_ret,u2_ret_en,
+  u3_A0,u3_B0,u3_A1,u3_B1,u3_en,u3_op,
+  u3_fufwd_A,u3_fuufwd_A,u3_fufwd_B,u3_fuufwd_B,
+  u3_ret,u3_ret,
+  u4_A0,u4_B0,u4_A1,u4_B1,u4_en,u4_op,
+  u4_fufwd_A,u4_fuufwd_A,u4_fufwd_B,u4_fuufwd_B,
+  u4_ret,u4_ret_en,
+  u5_A0,u5_B0,u5_A1,u5_B1,u5_en,u5_op,
+  u5_fufwd_A,u5_fuufwd_A,u5_fufwd_B,u5_fuufwd_B,
+  u5_ret,u5_ret,
+  u6_A0,u6_B0,u6_A1,u6_B1,u6_en,u6_op,
+  u6_fufwd_A,u6_fuufwd_A,u6_fufwd_B,u6_fuufwd_B,
+  u6_ret,u6_ret_en,
+  FUFH0,FUFH1,FUFH2,
+  FUFH3,FUFH4,FUFH5,
+  FUFH6,FUFH7,FUFH8,
+  FUFH9,
+  FUFL0,FUFL1,FUFL2,
+  FUFL3,FUFL4,FUFL5,
+  FUFL6,FUFL7,FUFL8,
+  FUFL9,
+  ALTDATAH0,ALTDATAH1,
+  ALTDATAL0,ALTDATAL1,
+  ALT_INP
+  );
+
   get_LDQ_new_en ldq_new_mod(
   rs_port[0],rs_ldst_flg[0], 
   rs_port[3],rs_ldst_flg[3], 
