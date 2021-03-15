636fb4bee64aeb4ebf71f9ad71750103b9dd0119
Goran Dakov <goran.dakov@gmail.com>
2021-03-15 14:04:53 -0400

    added some stuff to various places

--- rtl/frontend/frontend1.sv
+++ rtl/frontend/frontend1.sv
@@ -365,6 +365,7 @@ module frontend1(
   wire [3:0] jdec_push;
   wire [3:0] jdec_pop;
   wire [3:0][4:0] jdec_type;
+  wire [3:0][3:0] jdec_attr;
   wire [4:0] jdec_link0;
   wire [4:0] jdec_link1;
   wire [4:0] jdec_link2;
@@ -614,14 +615,36 @@ module frontend1(
   wire btb_hold_except;
   reg read_set_flag,read_set_flag_reg;
   
+  reg [3:0] btbx_attr0_reg;
+  reg [3:0] btbx_attr1_reg;
+  reg [3:0] btbx_attr2_reg;
+  reg [3:0] btbx_attr3_reg;
+  reg [3:0] btbx_attr0_reg2;
+  reg [3:0] btbx_attr1_reg2;
+  reg [3:0] btbx_attr2_reg2;
+  reg [3:0] btbx_attr3_reg2;
+  reg [3:0] btbx_attr0_reg3;
+  reg [3:0] btbx_attr1_reg3;
+  reg [3:0] btbx_attr2_reg3;
+  reg [3:0] btbx_attr3_reg3;
+  reg [3:0] btbx_attr0_reg4;
+  reg [3:0] btbx_attr1_reg4;
+  reg [3:0] btbx_attr2_reg4;
+  reg [3:0] btbx_attr3_reg4;
 //  wire tr_odd_d,tr_half_d;
 //  reg tr_odd,tr_odd_reg,tr_odd_reg2;
 //  reg tr_half,tr_half_reg,tr_half_reg2;
 
   wire jlnin,jlnint,jlninx;  
       
-  wire [16:0] lpar0;
-  wire [16:0] lpar1;
+  wire [6:0] lpar30;
+  wire [6:0] lpar31;
+  wire [6:0] lpar20;
+  wire [6:0] lpar21;
+  wire [6:0] lpar10;
+  wire [6:0] lpar11;
+  wire [6:0] lpar00;
+  wire [6:0] lpar01;
  // wire [5:0] lcpar0;
   //wire [5:0] lcpar1;
 //  wire instrEn2=instr
@@ -1150,7 +1173,7 @@ module frontend1(
 
   adder #(5) ln0off1t_mod(lnk_off0_reg,lnk_link0_reg,jdec_link0,1'b1,lnk_magic0_reg[0] & ~lnk_isRet0_reg,,,,);
   adder_CSA #(5) ln0off2CSAt_mod(lnk_off0_reg,lnk_link0_reg,5'd2,lpar00,lpar01);
-  adder #(5) ln0off2t_mod(lpar0[4:0],lpar1[4:0],jdec_link0,1'b0,~lnk_magic0_reg[0] & ~lnk_isRet0_reg,,,,);
+  adder #(5) ln0off2t_mod(lpar00[4:0],lpar01[4:0],jdec_link0,1'b0,~lnk_magic0_reg[0] & ~lnk_isRet0_reg,,,,);
 
   adder #(5) ln1off1t_mod(lnk_off1_reg,lnk_link1_reg,jdec_link1,1'b1,lnk_magic1_reg[0] & ~lnk_isRet1_reg,,,,);
   adder_CSA #(5) ln1off2CSAt_mod(lnk_off1_reg,lnk_link1_reg,5'd2,lpar10,lpar11);
@@ -1166,10 +1189,10 @@ module frontend1(
 
   get_carry #(4) btbLNoffCmpCC(lnk_off0_reg,~cc_read_IP_reg2[4:1],1'b1,lnk_offIn_cc);
   
-  get_carry #(4) btbL0NoffCmpCC(btb_jlnoff0,~cc_read_IP[4:1],1'b1,btb_jlnin0);
-  get_carry #(4) btbL1NoffCmpCC(btb_jlnoff1,~cc_read_IP[4:1],1'b1,btb_jlnin1);
-  get_carry #(4) btbL2NoffCmpCC(btb_jlnoff2,~cc_read_IP[4:1],1'b1,btb_jlnin2);
-  get_carry #(4) btbL3NoffCmpCC(btb_jlnoff3,~cc_read_IP[4:1],1'b1,btb_jlnin3);
+  get_carry #(4) btbL0NoffCmpCC(btb_jlnpos0,~cc_read_IP[4:1],1'b1,btb_jlnin0);
+  get_carry #(4) btbL1NoffCmpCC(btb_jlnpos1,~cc_read_IP[4:1],1'b1,btb_jlnin1);
+  get_carry #(4) btbL2NoffCmpCC(btb_jlnpos2,~cc_read_IP[4:1],1'b1,btb_jlnin2);
+  get_carry #(4) btbL3NoffCmpCC(btb_jlnpos3,~cc_read_IP[4:1],1'b1,btb_jlnin3);
   
   adder #(9) baseTick_mod(cc_read_IP[13:5],~cc_base_IP[13:5],{cc_base_tick,cc_base_off},1'b1,do_seq_reg,,,,);
   adder_inc #(35) baseInc_mod(cc_base_IP[47:13],cc_base_IP_d[47:13],do_seq_reg & cc_base_tick,);
@@ -1227,7 +1250,7 @@ module frontend1(
   .way(btb_way),
   .off0(btb_joff[0]),.off1(btb_joff[1]),.off2(btb_joff[2]),.off3(btb_joff[3]),
   .attr0I(jdec_attr[0]),.attr1I(jdec_attr[1]),.attr2I(jdec_attr[2]),.attr3I(jdec_attr[3]),
-  .attr0O(btb_attr0),attr1O(btb_attr1),attr2O(btb_attr2),attr3O(btb_attr3),
+  .attr0O(btb_attr0),.attr1O(btb_attr1),.attr2O(btb_attr2),.attr3O(btb_attr3),
   .tgt0I(jdec_target[0]),.tgt1I(jdec_target[1]),.tgt2I(jdec_target[2]),.tgt3I(jdec_target[3]),
   .tgt0O(btb_tgt0),
   .tgt1O(btb_tgt1),
