a1256263b330a67df5b6e17540c746175c960652
Goran Dakov <goran.dakov@gmail.com>
2021-04-17 15:41:33 -0400

    Revert "began sizify (x86_64 like)"
    This reverts commit 7cb2b958ab66bb15b3019b7bb3cd5c57b490d2e0.

--- rtl/transl/opcode.sv
+++ rtl/transl/opcode.sv
@@ -6,17 +6,9 @@ module ttag_is_opcode(
 
   always @(data) begin
       if (data[15:0]==16'h3af0) begin
-	  opc={2'b11,data[23:16]};
-	  bytes=4;
       end else if (data[15:0]==16'h3af0) begin
-	  opc={2'b10,data[23:16]};
-	  bytes=4;
       end else if (data[7:0]==8'hf0) begin
-	  opc={2'b1,data[15:8]};
-	  bytes=2;
       end else begin
-	  opc={2'b0,data[7:0]};
-	  bytes=1;
       end
   end
   
