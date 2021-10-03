ac6b5c6bdd6a3db8276caa8bedccd267e6b49806
Goran Dakov <goran.dakov@gmail.com>
2021-10-03 19:43:01 +0100

    added some stuff to dc1 ptr modules

--- rtl/cache/dc1_spectr.sv
+++ rtl/cache/dc1_spectr.sv
@@ -105,6 +105,15 @@ module dc1_xbit(
   wire [35:0] readB_data_ram[1:0];
   wire [35:0] write_dataA;
   wire [35:0] write_dataB;
+  
+  wire [31:0] read0_data_ramx;
+  wire [31:0] read1_data_ramx;
+  wire [31:0] read2_data_ramx;
+  wire [31:0] read3_data_ramx;
+  wire [31:0] readA_data_ramx;
+  wire [31:0] readB_data_ramx;
+  wire [31:0] write_dataAx;
+  wire [31:0] write_dataBx;
 
   reg write0_clkEn_reg[1:0];
   reg write1_clkEn_reg[1:0];
@@ -113,6 +122,14 @@ module dc1_xbit(
   reg [ADDR_WIDTH+4:0] write1_addr_reg;
   reg [ADDR_WIDTH+4:0] write0_addr_reg;
 
+function [35:0] en_ECC;
+  input [31:0] data;
+  en_ECC={^data[31:24],data[31:24],^data[23:16],data[23:16],^data[15:8],data[15:8],^data[7:0],data[7:0]};
+endfunction
+function [31:0] un_ECC;
+  input [35:0] data;
+  un_ECC={data[34:27],data[25:18],data[16:9],data[7:0]};
+endfunction
 generate
   genvar x;
   for(x=0;x<2;x=x+1)begin : ramset
@@ -164,8 +181,67 @@ generate
   write1_clkEn_reg[x]&~write_ins_reg[x],
   write1_addr_reg[ADDR_WIDTH+3:4],
   write_dataB);
+
   end
 endgenerate
 
+assign write_dataAx=write_ins_reg!=2'b0 ? {write_data_reg,write_data_reg} : 
+	write0_data_pbit_reg<<{write0_addr_reg[ADDR_WIDTH+4],write0_addr_reg[3:0]};
+assign write_dataBx=write1_data_pbit_reg<<{write1_addr_reg[ADDR_WIDTH+4],write1_addr_reg[3:0]};
 
+assign write_dataA[17:0]=write0_addr_reg[ADDR_WIDTH+4] ? en_ECC(read_dataA_ramx[17:0]) : en_ECC(write_dataAx)[17:0];
+assign write_dataA[35:18]=~write0_addr_reg[ADDR_WIDTH+4] ? en_ECC(read_dataA_ramx[35:18]) : en_ECC(write_dataAx)[35:18];
+assign write_dataB[17:0]=write1_addr_reg[ADDR_WIDTH+4] ? en_ECC(read_dataB_ramx[17:0]) : en_ECC(write_dataBx)[17:0];
+assign write_dataB[35:18]=~write1_addr_reg[ADDR_WIDTH+4] ? en_ECC(read_dataB_ramx[35:18]) : en_ECC(write_dataBx)[35:18];
+  
+assign read0_data_ramx=un_ECC(read0_data_ram[read0_odd_reg]);
+assign read1_data_ramx=un_ECC(read1_data_ram[read1_odd_reg]);
+assign read2_data_ramx=un_ECC(read2_data_ram[read2_odd_reg]);
+assign read3_data_ramx=un_ECC(read3_data_ram[read3_odd_reg]);
+assign readA_data_ramx=un_ECC(readA_data_ram[write0_odd_reg]);
+assign readB_data_ramx=un_ECC(readB_data_ram[write1_odd_reg]);
+
+assign read0_pbit=read0_data_ramx[{read0_addr_reg[ADDR_WIDTH+4],read0_addr_reg[3:0]}];
+assign read1_pbit=read1_data_ramx[{read1_addr_reg[ADDR_WIDTH+4],read1_addr_reg[3:0]}];
+assign read2_pbit=read2_data_ramx[{read2_addr_reg[ADDR_WIDTH+4],read2_addr_reg[3:0]}];
+assign read3_pbit=read3_data_ramx[{read3_addr_reg[ADDR_WIDTH+4],read3_addr_reg[3:0]}];
+
+always @(posedge clk) begin
+  if (rst) begin
+      write0_clkEn_reg<=2'b0;
+      write1_clkEn_reg<=2'b0;
+      write_ins_reg<=2'b0;
+      write_data_reg<=0;
+      write0_addr_reg<=0;
+      write1_addr_reg<=0;
+      read0_addr_reg<=0;
+      read1_addr_reg<=0;
+      read2_addr_reg<=0;
+      read3_addr_reg<=0;
+      read0_odd_reg<=1'b0;
+      read1_odd_reg<=1'b0;
+      read2_odd_reg<=1'b0;
+      read3_odd_reg<=1'b0;
+      write0_odd_reg<=1'b0;
+      write1_odd_reg<=1'b0;
+
+  end else begin
+      write0_clkEn_reg<={write0_odd,~write0_odd}&{2{write0_clkEn}};
+      write1_clkEn_reg<={write1_odd,~write1_odd}&{2{write1_clkEn}};
+      write_ins_reg<={write0_odd,~write0_odd}&{2{write_ins}};
+      write_data_reg<=write_data;
+      write0_addr_reg<=write0_addr;
+      write1_addr_reg<=write1_addr;
+      read0_addr_reg<=read0_addr;
+      read1_addr_reg<=read1_addr;
+      read2_addr_reg<=read2_addr;
+      read3_addr_reg<=read3_addr;
+      read0_odd_reg<=read0_odd;
+      read1_odd_reg<=read1_odd;
+      read2_odd_reg<=read2_odd;
+      read3_odd_reg<=read3_odd;
+      write0_odd_reg<=write0_odd;
+      write1_odd_reg<=write1_odd;
+  end
+end
 endmodule
