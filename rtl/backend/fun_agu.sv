f9a4b78843d30c8c43b56c77e421f7bb5840bf64
Goran Dakov <goran.dakov@gmail.com>
2021-12-01 14:43:59 +0000

    added some stuff to fun_agu

--- rtl/backend/fun_agu.sv
+++ rtl/backend/fun_agu.sv
@@ -2442,8 +2442,8 @@ module agu_block(
   .unlock(miss_unlock)
   );
 
-  assign alt_bus_hold=insert_isData_reg2;
-  assign alt_bus_addr=insBus_addr_reg2;
+  assign alt_bus_hold=insert_isData_reg;
+  assign alt_bus_addr=insBus_addr_reg;
 
   assign p0_adata[`lsaddr_addrE]=mOpX0_addrEven_reg;
   assign p0_adata[`lsaddr_addrO]=mOpX0_addrOdd_reg;
