0ede50f352166f3fdccf01e151583f2b82a37e4f
Goran Dakov <goran.dakov@gmail.com>
2022-01-02 08:54:23 +0000

    added some stuff

--- rtl/cams/l1dmisscam.sv
+++ rtl/cams/l1dmisscam.sv
@@ -914,6 +914,7 @@ module dmisscam(
 	  pwned<=pwned|read_en_way;
 	  if (unlock) pwned<=16'b0;
       end
+      $display("slp ",started,locked,pwned,unlock,begin_replay,read_en_way," RA ",read_addr)
       if (rst) begin
           fill_en0_reg<=0;
           fill_addrE0_reg<=0;
