b0ef69fa221ddd59da908be86ba7229972d9d6ea
Goran Dakov <goran.dakov@gmail.com>
2022-03-13 12:45:52 +0000

    indverted chkMask{0|1|2} in ldq_buf cmp

--- rtl/lsq/ldq.sv
+++ rtl/lsq/ldq.sv
@@ -190,10 +190,10 @@ module ldq_buf(
   assign chkMatch2=chkMatch[2]&~free&chkEn2;
   assign chkMatch3=chkMatch[3]&~free&chkEn3;
 
-  assign freeConfl0=(((freeII0==II && confl!=0) || (((chkMask0&freeMask0)!=0 || (chkMask1&freeMask0)!=0 || 
-    (chkMask2&freeMask0)!=0) && freeII0[9:4]==II[9:4])) && ~free) &freeEn0;
-  assign freeConfl1=(((freeII1==II && confl!=0) || (((chkMask0&freeMask1)!=0 || (chkMask1&freeMask1)!=0 || 
-    (chkMask2&freeMask1)!=0) && freeII1[9:4]==II[9:4])) && ~free) &freeEn1;
+  assign freeConfl0=(((freeII0==II && confl!=0) || (((~chkMask0&freeMask0)!=0 || (~chkMask1&freeMask0)!=0 || 
+    (~chkMask2&freeMask0)!=0) && freeII0[9:4]==II[9:4])) && ~free) &freeEn0;
+  assign freeConfl1=(((freeII1==II && confl!=0) || (((~chkMask0&freeMask1)!=0 || (~chkMask1&freeMask1)!=0 || 
+    (~chkMask2&freeMask1)!=0) && freeII1[9:4]==II[9:4])) && ~free) &freeEn1;
 
   assign freeConflSmp0=(freeII0==II && confl_smp) & freeEn0;
   assign freeConflSmp1=(freeII1==II && confl_smp) & freeEn1;
