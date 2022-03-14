a2365884c8bb7721c4b5a6515da0605050aaf95e
Goran Dakov <goran.dakov@gmail.com>
2022-03-14 17:58:03 +0000

    changed stq license to apache 2.0 because its similar to Sonic Boom stq

--- rtl/lsq/stq_data.sv
+++ rtl/lsq/stq_data.sv
@@ -1,5 +1,19 @@
 `include "../struct.sv"
+/*
+Copyright 2022 Goran Dakov
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 module stq_data(
   clk,
   rst,
@@ -88,7 +102,31 @@ module stq_data_array(
         wire [WIDTH-1:0] chk4_dataK;
         wire [WIDTH-1:0] chk5_dataK;
         for(row=0;row<8;row=row+1) begin : row_gen
+            stq_data(
+            clk,
+            rst,
+            wrt0_en[bank*8+row],wrt0_data,
+            wrt1_en[bank*8+row],wrt1_data,
+            chk0_en[bank*8+row],chk0_dataK,
+            chk1_en[bank*8+row],chk1_dataK,
+            chk2_en[bank*8+row],chk2_dataK,
+            chk3_en[bank*8+row],chk3_dataK,
+            chk4_en[bank*8+row],chk4_dataK,
+            chk5_en[bank*8+row],chk5_dataK);
         end
+        assign chk0_dataK=|chk0_en[bank*8+:8] ? {WIDTH{1'BZ}} : {WIDTH{1'B0}};
+        assign chk1_dataK=|chk1_en[bank*8+:8] ? {WIDTH{1'BZ}} : {WIDTH{1'B0}};
+        assign chk2_dataK=|chk2_en[bank*8+:8] ? {WIDTH{1'BZ}} : {WIDTH{1'B0}};
+        assign chk3_dataK=|chk3_en[bank*8+:8] ? {WIDTH{1'BZ}} : {WIDTH{1'B0}};
+        assign chk4_dataK=|chk4_en[bank*8+:8] ? {WIDTH{1'BZ}} : {WIDTH{1'B0}};
+        assign chk5_dataK=|chk5_en[bank*8+:8] ? {WIDTH{1'BZ}} : {WIDTH{1'B0}};
+
+        assign chk0_data=|chk0_en[bank*8+:8] ? chk0_dataK : {WIDTH{1'BZ}};
+        assign chk1_data=|chk1_en[bank*8+:8] ? chk1_dataK : {WIDTH{1'BZ}};
+        assign chk2_data=|chk2_en[bank*8+:8] ? chk2_dataK : {WIDTH{1'BZ}};
+        assign chk3_data=|chk3_en[bank*8+:8] ? chk3_dataK : {WIDTH{1'BZ}};
+        assign chk4_data=|chk4_en[bank*8+:8] ? chk4_dataK : {WIDTH{1'BZ}};
+        assign chk5_data=|chk5_en[bank*8+:8] ? chk5_dataK : {WIDTH{1'BZ}};
     end
   endgenerate
 endmodule  
