967642f2961ad097813374e24d219439143cab5e
Goran Dakov <goran.dakov@gmail.com>
2022-03-16 13:32:55 +0000

    added some stuff to stq

--- rtl/lsq/stq.sv
+++ rtl/lsq/stq.sv
@@ -49,57 +49,49 @@ module stq(
       for(b=0;b<4;b=b+1) begin : L
           assign chk0_subBNK[b]={chk0_banks[28+b],chk0_banks[24+b],chk0_banks[20+b],chk0_banks[16+b],
               chk0_banks[12+b],chk0_banks[8+b],chk0_banks[4+b],chk0_banks[0+b]};
-          assign chk0_banks=(chk0_adata[`lsaddr_sz]==5'h11 || chk0_adata[`lsaddr_sz]==5'h10 || chk0_adata[`lsaddr_low]==2'b0) ?
-            chk0_adata[`lsaddr_banks] : lowt(chk0_adata[`lsaddr_banks]);
+          assign chk0_odd1[b]=chk0_banks[28+b] && |chk0_banks[3:0] ? chk0_odd0^3'b1 : chk0_odd0;
 
           assign chk1_subBNK[b]={chk1_banks[28+b],chk1_banks[24+b],chk1_banks[20+b],chk1_banks[16+b],
               chk1_banks[12+b],chk1_banks[8+b],chk1_banks[4+b],chk1_banks[0+b]};
-          assign chk1_banks=(chk1_adata[`lsaddr_sz]==5'h11 || chk1_adata[`lsaddr_sz]==5'h10 || chk1_adata[`lsaddr_low]==2'b0) ?
-            chk1_adata[`lsaddr_banks] : lowt(chk1_adata[`lsaddr_banks]);
+          assign chk1_odd1[b]=chk1_banks[28+b] && |chk1_banks[3:0] ? chk1_odd0^3'b1 : chk1_odd0;
 
           assign chk2_subBNK[b]={chk2_banks[28+b],chk2_banks[24+b],chk2_banks[20+b],chk2_banks[16+b],
               chk2_banks[12+b],chk2_banks[8+b],chk2_banks[4+b],chk2_banks[0+b]};
-          assign chk2_banks=(chk2_adata[`lsaddr_sz]==5'h11 || chk2_adata[`lsaddr_sz]==5'h10 || chk2_adata[`lsaddr_low]==2'b0) ?
-            chk2_adata[`lsaddr_banks] : lowt(chk2_adata[`lsaddr_banks]);
+          assign chk2_odd1[b]=chk2_banks[28+b] && |chk2_banks[3:0] ? chk2_odd0^3'b1 : chk2_odd0;
 
           assign chk3_subBNK[b]={chk3_banks[28+b],chk3_banks[24+b],chk3_banks[20+b],chk3_banks[16+b],
               chk3_banks[12+b],chk3_banks[8+b],chk3_banks[4+b],chk3_banks[0+b]};
-          assign chk3_banks=(chk3_adata[`lsaddr_sz]==5'h11 || chk3_adata[`lsaddr_sz]==5'h10 || chk3_adata[`lsaddr_low]==2'b0) ?
-            chk3_adata[`lsaddr_banks] : lowt(chk3_adata[`lsaddr_banks]);
+          assign chk3_odd1[b]=chk3_banks[28+b] && |chk3_banks[3:0] ? chk3_odd0^3'b1 : chk3_odd0;
 
           assign chk4_subBNK[b]={chk4_banks[28+b],chk4_banks[24+b],chk4_banks[20+b],chk4_banks[16+b],
               chk4_banks[12+b],chk4_banks[8+b],chk4_banks[4+b],chk4_banks[0+b]};
-          assign chk4_banks=(chk4_adata[`lsaddr_sz]==5'h11 || chk4_adata[`lsaddr_sz]==5'h10 || chk4_adata[`lsaddr_low]==2'b0) ?
-            chk4_adata[`lsaddr_banks] : lowt(chk4_adata[`lsaddr_banks]);
+          assign chk4_odd1[b]=chk4_banks[28+b] && |chk4_banks[3:0] ? chk4_odd0^3'b1 : chk4_odd0;
 
           assign chk5_subBNK[b]={chk5_banks[28+b],chk5_banks[24+b],chk5_banks[20+b],chk5_banks[16+b],
               chk5_banks[12+b],chk5_banks[8+b],chk5_banks[4+b],chk5_banks[0+b]};
-          assign chk5_banks=(chk5_adata[`lsaddr_sz]==5'h11 || chk5_adata[`lsaddr_sz]==5'h10 || chk5_adata[`lsaddr_low]==2'b0) ?
-            chk5_adata[`lsaddr_banks] : lowt(chk5_adata[`lsaddr_banks]);
+          assign chk5_odd1[b]=chk5_banks[28+b] && |chk5_banks[3:0] ? chk5_odd0^3'b1 : chk5_odd0;
           
           assign wrt0_subBNK[b]={wrt0_banks[28+b],wrt0_banks[24+b],wrt0_banks[20+b],wrt0_banks[16+b],
               wrt0_banks[12+b],wrt0_banks[8+b],wrt0_banks[4+b],wrt0_banks[0+b]};
-          assign wrt0_banks=(wrt0_adata[`lsaddr_sz]==5'h11 || wrt0_adata[`lsaddr_sz]==5'h10 || wrt0_adata[`lsaddr_low]==2'b0) ?
-            wrt0_adata[`lsaddr_banks] : lowt(wrt0_adata[`lsaddr_banks]);
+          assign wrt0_odd1[b]=wrt0_banks[28+b] && |wrt0_banks[3:0] ? wrt0_odd0^3'b1 : wrt0_odd0;
 
           assign wrt1_subBNK[b]={wrt1_banks[28+b],wrt1_banks[24+b],wrt1_banks[20+b],wrt1_banks[16+b],
               wrt1_banks[12+b],wrt1_banks[8+b],wrt1_banks[4+b],wrt1_banks[0+b]};
-          assign wrt1_banks=(wrt1_adata[`lsaddr_sz]==5'h11 || wrt1_adata[`lsaddr_sz]==5'h10 || wrt1_adata[`lsaddr_low]==2'b0) ?
-            wrt1_adata[`lsaddr_banks] : lowt(wrt1_adata[`lsaddr_banks]);
+          assign wrt1_odd1[b]=wrt1_banks[28+b] && |wrt1_banks[3:0] ? wrt1_odd0^3'b1 : wrt1_odd0;
 
           stq_buf_L_array arr0_mod(
           clk,
           rst,
           aStall|aDoStall,
           excpt,
-          wrt0_en0, wrt0_addrEO[b], wrt0_odd[b], wrt0_bytes[b], wrt0_subBNK[b],
-          wrt1_en0, wrt1_addrEO[b], wrt1_odd[b], wrt1_bytes[b], wrt1_subBNK[b],
-          chk0_en, chk0_addrEO[b], chk0_odd[b], chk0_bytes[b], chk0_subBNK[b], chk0_match[b][31:0], chk0_partial[b][31:0],
-          chk1_en, chk1_addrEO[b], chk1_odd[b], chk1_bytes[b], chk1_subBNK[b], chk1_match[b][31:0], chk1_partial[b][31:0],
-          chk2_en, chk2_addrEO[b], chk2_odd[b], chk2_bytes[b], chk2_subBNK[b], chk2_match[b][31:0], chk2_partial[b][31:0],
-          chk3_en, chk3_addrEO[b], chk3_odd[b], chk3_bytes[b], chk3_subBNK[b], chk3_match[b][31:0], chk3_partial[b][31:0],
-          chk4_en, chk4_addrEO[b], chk4_odd[b], chk4_bytes[b], chk4_subBNK[b], chk4_match[b][31:0], chk4_partial[b][31:0],
-          chk5_en, chk5_addrEO[b], chk5_odd[b], chk5_bytes[b], chk5_subBNK[b], chk5_match[b][31:0], chk5_partial[b][31:0],
+          wrt0_en0, wrt0_addrEO[b], wrt0_odd1[b], wrt0_bytes[b], wrt0_subBNK[b],
+          wrt1_en0, wrt1_addrEO[b], wrt1_odd1[b], wrt1_bytes[b], wrt1_subBNK[b],
+          chk0_en, chk0_addrEO[b], chk0_odd1[b], chk0_bytes[b], chk0_subBNK[b], chk0_match[b][31:0], chk0_partial[b][31:0],
+          chk1_en, chk1_addrEO[b], chk1_odd1[b], chk1_bytes[b], chk1_subBNK[b], chk1_match[b][31:0], chk1_partial[b][31:0],
+          chk2_en, chk2_addrEO[b], chk2_odd1[b], chk2_bytes[b], chk2_subBNK[b], chk2_match[b][31:0], chk2_partial[b][31:0],
+          chk3_en, chk3_addrEO[b], chk3_odd1[b], chk3_bytes[b], chk3_subBNK[b], chk3_match[b][31:0], chk3_partial[b][31:0],
+          chk4_en, chk4_addrEO[b], chk4_odd1[b], chk4_bytes[b], chk4_subBNK[b], chk4_match[b][31:0], chk4_partial[b][31:0],
+          chk5_en, chk5_addrEO[b], chk5_odd1[b], chk5_bytes[b], chk5_subBNK[b], chk5_match[b][31:0], chk5_partial[b][31:0],
           upd0_en0[31:0], 
           upd1_en0[31:0], 
           free_en[31:0],free[31:0],upd[31:0],passe[31:0],passe_en[31:0]);end
@@ -163,4 +155,35 @@ module stq(
           assign passe_en[x]=(pse0_WQ==x && pse0_en) || (pse1_WQ==x && pse1_en);
       end
   endgenerate
+  assign chk0_banks=(chk0_adata[`lsaddr_sz]==5'h11 || chk0_adata[`lsaddr_sz]==5'h10 || chk0_adata[`lsaddr_low]==2'b0) ?
+    chk0_adata[`lsaddr_banks] : lowt(chk0_adata[`lsaddr_banks]);
+  assign chk0_odd0=(chk0_adata[`lsaddr_sz]==5'h11 || chk0_adata[`lsaddr_sz]==5'h10) ? {2'b0,chk0_odd} : {chk0_adata{`lsaddr_low],chk0_odd}; 
+
+  assign chk1_banks=(chk1_adata[`lsaddr_sz]==5'h11 || chk1_adata[`lsaddr_sz]==5'h10 || chk1_adata[`lsaddr_low]==2'b0) ?
+    chk1_adata[`lsaddr_banks] : lowt(chk1_adata[`lsaddr_banks]);
+  assign chk1_odd0=(chk1_adata[`lsaddr_sz]==5'h11 || chk1_adata[`lsaddr_sz]==5'h10) ? {2'b0,chk1_odd} : {chk1_adata{`lsaddr_low],chk1_odd}; 
+
+  assign chk2_banks=(chk2_adata[`lsaddr_sz]==5'h11 || chk2_adata[`lsaddr_sz]==5'h10 || chk2_adata[`lsaddr_low]==2'b0) ?
+    chk2_adata[`lsaddr_banks] : lowt(chk2_adata[`lsaddr_banks]);
+  assign chk2_odd0=(chk2_adata[`lsaddr_sz]==5'h11 || chk2_adata[`lsaddr_sz]==5'h10) ? {2'b0,chk2_odd} : {chk2_adata{`lsaddr_low],chk2_odd}; 
+
+  assign chk3_banks=(chk3_adata[`lsaddr_sz]==5'h11 || chk3_adata[`lsaddr_sz]==5'h10 || chk3_adata[`lsaddr_low]==2'b0) ?
+    chk3_adata[`lsaddr_banks] : lowt(chk3_adata[`lsaddr_banks]);
+  assign chk3_odd0=(chk3_adata[`lsaddr_sz]==5'h11 || chk3_adata[`lsaddr_sz]==5'h10) ? {2'b0,chk3_odd} : {chk3_adata{`lsaddr_low],chk3_odd}; 
+
+  assign chk4_banks=(chk4_adata[`lsaddr_sz]==5'h11 || chk4_adata[`lsaddr_sz]==5'h10 || chk4_adata[`lsaddr_low]==2'b0) ?
+    chk4_adata[`lsaddr_banks] : lowt(chk4_adata[`lsaddr_banks]);
+  assign chk4_odd0=(chk4_adata[`lsaddr_sz]==5'h11 || chk4_adata[`lsaddr_sz]==5'h10) ? {2'b0,chk4_odd} : {chk4_adata{`lsaddr_low],chk4_odd}; 
+
+  assign chk5_banks=(chk5_adata[`lsaddr_sz]==5'h11 || chk5_adata[`lsaddr_sz]==5'h10 || chk5_adata[`lsaddr_low]==2'b0) ?
+    chk5_adata[`lsaddr_banks] : lowt(chk5_adata[`lsaddr_banks]);
+  assign chk5_odd0=(chk5_adata[`lsaddr_sz]==5'h11 || chk5_adata[`lsaddr_sz]==5'h10) ? {2'b0,chk5_odd} : {chk5_adata{`lsaddr_low],chk5_odd}; 
+  
+  assign wrt0_banks=(wrt0_adata[`lsaddr_sz]==5'h11 || wrt0_adata[`lsaddr_sz]==5'h10 || wrt0_adata[`lsaddr_low]==2'b0) ?
+    wrt0_adata[`lsaddr_banks] : lowt(wrt0_adata[`lsaddr_banks]);
+  assign wrt0_odd0=(wrt0_adata[`lsaddr_sz]==5'h11 || wrt0_adata[`lsaddr_sz]==5'h10) ? {2'b0,wrt0_odd} : {wrt0_adata{`lsaddr_low],wrt0_odd}; 
+
+  assign wrt1_banks=(wrt1_adata[`lsaddr_sz]==5'h11 || wrt1_adata[`lsaddr_sz]==5'h10 || wrt1_adata[`lsaddr_low]==2'b0) ?
+    wrt1_adata[`lsaddr_banks] : lowt(wrt1_adata[`lsaddr_banks]);
+  assign wrt1_odd0=(wrt1_adata[`lsaddr_sz]==5'h11 || wrt1_adata[`lsaddr_sz]==5'h10) ? {2'b0,wrt1_odd} : {wrt1_adata{`lsaddr_low],wrt1_odd}; 
 endmodule
