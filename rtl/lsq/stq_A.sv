34a24e31fe75708c9fa7e8ecd67e4c7dd3270420
Goran Dakov <goran.dakov@gmail.com>
2022-03-18 17:29:51 +0000

    added stq_A_array module

--- rtl/lsq/stq_A.sv
+++ rtl/lsq/stq_A.sv
@@ -143,121 +143,93 @@ module stq_buf_A(
       end
   end
 endmodule
-
-module stq_buf_L_array(
+module stq_buf_A_array(
   clk,
   rst,
   stallA,
   excpt,
-  wrt0_en, wrt0_odd, wrt0_bytes, wrt0_subBNK, wrt0_subBNK2, 
-  wrt1_en, wrt1_odd, wrt1_bytes, wrt1_subBNK, wrt1_subBNK2, 
-  chk0_en, chk0_addrEO, chk0_odd, chk0_bytes, chk0_subBNK, chk0_subBNK2, chk0_match, chk0_partial,
-  chk1_en, chk1_addrEO, chk1_odd, chk1_bytes, chk1_subBNK, chk1_subBNK2, chk1_match, chk1_partial,
-  chk2_en, chk2_addrEO, chk2_odd, chk2_bytes, chk2_subBNK, chk2_subBNK2, chk2_match, chk2_partial,
-  chk3_en, chk3_addrEO, chk3_odd, chk3_bytes, chk3_subBNK, chk3_subBNK2, chk3_match, chk3_partial,
-  chk4_en, chk4_addrEO, chk4_odd, chk4_bytes, chk4_subBNK, chk4_subBNK2, chk4_match, chk4_partial,
-  chk5_en, chk5_addrEO, chk5_odd, chk5_bytes, chk5_subBNK, chk5_subBNK2, chk5_match, chk5_partial,
+  wrt0_en, wrt0_addrE, wrt0_addrO, 
+  wrt1_en, wrt1_addrE, wrt1_addrO, 
+  chk0_en, chk0_addrEO, chk0_addrE, chk0_addrO,
+  chk0_en, chk0_addrEO, chk0_addrE, chk0_addrO,
+  chk0_en, chk0_addrEO, chk0_addrE, chk0_addrO,
+  chk0_en, chk0_addrEO, chk0_addrE, chk0_addrO,
+  chk0_en, chk0_addrEO, chk0_addrE, chk0_addrO,
+  chk0_en, chk0_addrEO, chk0_addrE, chk0_addrO,
   upd0_en, 
   upd1_en, 
   free_en,free,upd,passe,passe_en);
-  localparam WIDTH=2;
-  localparam BUF_COUNT=32;//2 rows of 32
+  localparam WIDTH=36;
+  localparam BUF_COUNT=32;
   input clk;
   input rst;
   input stallA;
   input excpt;
   
   input [BUF_COUNT-1:0] wrt0_en;
-  input [3:0] wrt0_odd;
-  input [3:0] wrt0_bytes;
-  input [3:0] wrt0_subBNK;
-  input [3:0] wrt0_subBNK2;
+  input [WIDTH-1:0] wrt0_addrE;
+  input [WIDTH-1:0] wrt0_addrO;
 
   input [BUF_COUNT-1:0] wrt1_en;
-  input [3:0] wrt1_odd;
-  input [3:0] wrt1_bytes;
-  input [3:0] wrt1_subBNK;
-  input [3:0] wrt1_subBNK2;
+  input [WIDTH-1:0] wrt0_addrE;
+  input [WIDTH-1:0] wrt0_addrO;
 
   input chk0_en;
-  input [WIDTH-1:0] chk0_addrEO;
-  input [3:0] chk0_odd;
-  input [3:0] chk0_bytes;
-  input [3:0] chk0_subBNK;
-  input [3:0] chk0_subBNK2;
-  output [BUF_COUNT-1:0] chk0_match;
-  output [BUF_COUNT-1:0] chk0_partial;
+  output [BUF_COUNT-1:0] [1:0] chk0_addrEO;
+  input [WIDTH-1:0] chk0_addrE;
+  input [WIDTH-1:0] chk0_addrO;
 
   input chk1_en;
-  input [WIDTH-1:0] chk1_addrEO;
-  input [3:0] chk1_odd;
-  input [3:0] chk1_bytes;
-  input [3:0] chk1_subBNK;
-  input [3:0] chk1_subBNK2;
-  output [BUF_COUNT-1:0] chk1_match;
-  output [BUF_COUNT-1:0] chk1_partial;
-
+  output [BUF_COUNT-1:0] [1:0] chk1_addrEO;
+  input [WIDTH-1:0] chk1_addrE;
+  input [WIDTH-1:0] chk1_addrO;
+  
   input chk2_en;
-  input [WIDTH-1:0] chk2_addrEO;
-  input [3:0] chk2_odd;
-  input [3:0] chk2_bytes;
-  input [3:0] chk2_subBNK;
-  input [3:0] chk2_subBNK2;
-  output [BUF_COUNT-1:0] chk2_match;
-  output [BUF_COUNT-1:0] chk2_partial;
-
+  output [BUF_COUNT-1:0] [1:0] chk2_addrEO;
+  input [WIDTH-1:0] chk2_addrE;
+  input [WIDTH-1:0] chk2_addrO;
+  
   input chk3_en;
-  input [WIDTH-1:0] chk3_addrEO;
-  input [3:0] chk3_odd;
-  input [3:0] chk3_bytes;
-  input [3:0] chk3_subBNK;
-  input [3:0] chk3_subBNK2;
-  output [BUF_COUNT-1:0] chk3_match;
-  output [BUF_COUNT-1:0] chk3_partial;
-
+  output [BUF_COUNT-1:0] [1:0] chk3_addrEO;
+  input [WIDTH-1:0] chk3_addrE;
+  input [WIDTH-1:0] chk3_addrO;
+  
   input chk4_en;
-  input [WIDTH-1:0] chk4_addrEO;
-  input [3:0] chk4_odd;
-  input [3:0] chk4_bytes;
-  input [3:0] chk4_subBNK;
-  input [3:0] chk4_subBNK2;
-  output [BUF_COUNT-1:0] chk4_match;
-  output [BUF_COUNT-1:0] chk4_partial;
-
+  output [BUF_COUNT-1:0] [1:0] chk4_addrEO;
+  input [WIDTH-1:0] chk4_addrE;
+  input [WIDTH-1:0] chk4_addrO;
+  
   input chk5_en;
-  input [WIDTH-1:0] chk5_addrEO;
-  input [3:0] chk5_odd;
-  input [3:0] chk5_bytes;
-  input [3:0] chk5_subBNK;
-  input [3:0] chk5_subBNK2;
-  output [BUF_COUNT-1:0] chk5_match;
-  output [BUF_COUNT-1:0] chk5_partial;
+  output [BUF_COUNT-1:0] [1:0] chk5_addrEO;
+  input [WIDTH-1:0] chk5_addrE;
+  input [WIDTH-1:0] chk5_addrO;
+
 
   input [BUF_COUNT-1:0] upd0_en;
   input [BUF_COUNT-1:0] upd1_en;
 
-  input  [BUF_COUNT-1:0] free_en;
-  output [BUF_COUNT-1:0] free;
-  output [BUF_COUNT-1:0] upd;
-  output [BUF_COUNT-1:0] passe;
-  input  [BUF_COUNT-1:0] passe_en;
+  input [BUF_COUNT-1:0] free_en;
+  output [BUF_COUNT-1:0]  free;
+  output [BUF_COUNT-1:0]  upd;
+  output [BUF_COUNT-1:0]  passe;
+  input [BUF_COUNT-1:0]  passe_en;
   
   generate
       genvar t;
       for(t=0;t<BUF_COUNT;t=t+1) begin buf_gen
-          stq_buf_L buf_mod(
+          stq_buf_A buf_mod(
           clk,
           rst,
           stallA,
           excpt,
-          wrt0_en[t], wrt0_odd, wrt0_bytes, wrt0_subBNK, wrt0_subBNK2, 
-          wrt1_en[t], wrt1_odd, wrt1_bytes, wrt1_subBNK, wrt1_subBNK2, 
-          chk0_en, chk0_addrEO, chk0_odd, chk0_bytes, chk0_subBNK, chk0_subBNK2, chk0_match[t], chk0_partial[t],
-          chk1_en, chk1_addrEO, chk1_odd, chk1_bytes, chk1_subBNK, chk1_subBNK2, chk1_match[t], chk1_partial[t],
-          chk2_en, chk2_addrEO, chk2_odd, chk2_bytes, chk2_subBNK, chk2_subBNK2, chk2_match[t], chk2_partial[t],
-          chk3_en, chk3_addrEO, chk3_odd, chk3_bytes, chk3_subBNK, chk3_subBNK2, chk3_match[t], chk3_partial[t],
-          chk4_en, chk4_addrEO, chk4_odd, chk4_bytes, chk4_subBNK, chk4_subBNK2, chk4_match[t], chk4_partial[t],
-          chk5_en, chk5_addrEO, chk5_odd, chk5_bytes, chk5_subBNK, chk5_subBNK2, chk5_match[t], chk5_partial[t],
+          wrt0_en[t], wrt0_addrE, wrt0_addrO, 
+          wrt1_en[t], wrt1_addrE, wrt1_addrO, 
+          chk0_en, chk0_addrEO[t], chk0_addrE, chk0_addrO,
+          chk1_en, chk1_addrEO[t], chk1_addrE, chk1_addrO,
+          chk2_en, chk2_addrEO[t], chk2_addrE, chk2_addrO,
+          chk3_en, chk3_addrEO[t], chk3_addrE, chk3_addrO,
+          chk4_en, chk4_addrEO[t], chk4_addrE, chk4_addrO,
+          chk5_en, chk5_addrEO[t], chk5_addrE, chk5_addrO,
           upd0_en[t], 
           upd1_en[t], 
           free_en[t],free[t],upd[t],passe[t],passe_en[t]);
