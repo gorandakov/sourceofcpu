82177bcdb8f4a5c3d5abe70f90f4be0b9be5a97b
Goran Dakov <mbgoran@yahoo.com>
2022-04-06 21:11:31 +0100

    added some stuff to fr12

--- rtl/fr12/sz32.sv
+++ rtl/fr12/sz32.sv
@@ -1,115 +1,95 @@
-module sz1(
-  input [3:0] bgn;
-  output reg [6:2] sz;
-  );
 
-  always @(bgn) begin
-      sz={bgn==4'ha,1'b0,bgn==4'he,bgn[2:0]!=3'b0 && bgn!=4'he && bgn!=4'ha,
-	  bgn[2:0]==3'b0};
+module sz_submod8(
+  sizes,
+  bits
+);
+  input [15:0] sizes;
+  output [7+5:0] bits;
+  always @* begin
+  casex(sizes)
+    16'b00xx00xx00xx00xx:bits=13'haa;
+
+    16'b01xxxx00xx00xx00:bits=13'h154;
+    16'b00xx01xxxx00xx00:bits=13'h152;
+    16'b00xx00xx01xxxx00:bits=13'h14a;
+    16'b00xx00xx00xx01xx:bits=13'h12a;
+
+    16'b10xxxxxx00xx00xx:bits=13'ha8;
+    16'b00xx10xxxxxx00xx:bits=13'ha2;
+    16'b00xx00xx10xxxxxx:bits=13'h8a;
+    16'b00xx00xx00xx10xx:bits=13'h22a;
+    
+    16'b11xxxxxxxxxx00xx:bits=13'ha0;
+    16'b00xx11xxxxxxxxxx:bits=13'h82;
+    16'b00xx00xx11xxxxxx:bits=13'h20a;
+    16'b00xx00xx00xx11xx:bits=13'h82a;
+
+    //2x big
+    //3-3
+    16'b01xxxx01xxxx00xx:bits=13'ha4;
+    16'b01xxxx00xx01xxxx:bits=13'h94;
+    16'b01xxxx00xx00xx01:bits=13'h254;
+    16'b00xx01xxxx01xxxx:bits=13'h92;
+    16'b00xx01xxxx00xx01:bits=13'h252;
+    16'b00xx00xx01xxxx01:bits=13'h24a;
+    //4-4
+    16'b10xxxxxx10xxxxxx:bits=13'h88;
+    16'b10xxxxxx00xx10xx:bits=13'h228;
+    16'b00xx10xxxxxx10xx:bits=13'h222;
+    //6-6
+    16'b11xxxxxxxxxx11xx:bits=13'h820;
+    //3-4
+    16'b01xxxx10xxxxxx00:bits=13'h144;
+    16'b01xxxx00xx10xxxx:bits=13'h114;
+    16'b01xxxx00xx00xx10:bits=13'h454;
+    16'b00xx01xxxx00xx10:bits=13'h452;
+    16'b00xx01xxxx10xxxx:bits=13'h112;
+    16'b00xx00xx01xxxx10:bits=13'h44a;
+    //4-3
+    16'b10xxxxxx01xxxx00:bits=13'h148;
+    16'b10xxxxxx00xx01xx:bits=13'h128;
+    16'b00xx10xxxxxx01xx:bits=13'h122;
+    //3-6
+    16'b01xxxx11xxxxxxxx:bits=13'h104;
+    16'b01xxxx00xx11xxxx:bits=13'h414;
+    16'b01xxxx00xx00xx11:bits=13'h1054;
+    16'b00xx01xxxx00xx11:bits=13'h1052;
+    16'b00xx01xxxx11xxxx:bits=13'h412;
+    16'b00xx00xx01xxxx11:bits=13'h104a;
+    //6-3
+    16'b11xxxxxxxxxx01xx:bits=13'h120;
+    //4-6
+    16'b10xxxxxx11xxxxxx:bits=13'h208;
+    16'b10xxxxxx00xx11xx:bits=13'h828;
+    16'b00xx10xxxxxx11xx:bits=13'h822;
+    //6-4
+    16'b11xxxxxxxxxx10xx:bits=13'h220;
+  endcase
   end
 endmodule
 
-module sz2(
-  input [3:0] bgn0;
-  input [3:0] bgn2;
-  input [3:0] bgn3;
-  input [3:0] bgn4;
-  input [3:0] bgn6;
-  output [6:2] sz0;
-  output [6:2] sz1;
-  output [12:4] cnt1;
-  );
-  wire [6:2][6:2];
-  sz1(bgn0,sz0);
-  sz1(bgn2,sz[2]);
-  sz1(bgn3,sz[3]);
-  sz1(bgn4,sz[4]);
-  sz1(bgn6,sz[6]);
-  assign sz[5]=5'd3;
+module sz32(
+  offsz,
+  sizes,
+  outsz,
+  bits);
+  parameter WIDTH_IN_BYTES=8;
+
   generate
-    genvar th;
-    for(th=2;th<7;th++) begin
-	assign cnt1=sz0[th] ? {5'd0,sz[th]}<<(th-2) : 10'bz;
-	assign sz1=sz0[th] ? sz[th] : 10'bz;
-    end
+      genvar k;
+      if (WIDTH_IN_BYTES>8) begin : pair
+	  sz32 #(WIDTH_IN_BYTES/2) sz_A_mod(offsz,sizes[WIDTH_IN_BYTES-1:0],outsz_A,bits[WIDTH_IN_BYTES/2-1:0]);
+	  sz32 #(WIDTH_IN_BYTES/2) sz_B_mod(outsz_A,sizes[WIDTH_IN_BYTES*2-1:WIDTH_IN_BYTES],outsz,
+		  bits[WIDTH_IN_BYTES-1:WIDTH_IN_BYTES/2]);
+      end else begin
+	  for(k=0;k<5;k=k+1) begin : assignos
+	      wire [5:0] first;
+	      wire has;
+	      sz_submod8({{k{2'b11}},sizes[WIDTH_IN_BYTES*2-1:k*2]},bitz[k]);
+	      assign bits=offsz[k] ? bitz[WIDTH_IN_BYTES-1:0]<<k : {WIDTH_IN_BYTES{1'bz}};
+	      bit_find_first_bit #(6) find_mmod(bitz[WIDTH_IN_BYTES+5-k:WIDTH_IN_BYTES-k],first,has);
+	      assign outsz=offsz[k] ? first : 6'bz;
+	  end
+      end
   endgenerate
-
 endmodule
-
-module sz2(
-  input [3:0] bgn0;
-  input [3:0] bgn2;
-  input [3:0] bgn3;
-  input [3:0] bgn4;
-  input [3:0] bgn5;
-  input [3:0] bgn6;
-  input [3:0] bgn7;
-  input [3:0] bgn8;
-  input [3:0] bgn9;
-  input [3:0] bgn10;
-  input [3:0] bgn11;
-  input [3:0] bgn12;
-  input [3:0] bgn13;
-  input [3:0] bgn14;
-  input [3:0] bgn15;
-  input [3:0] bgn16;
-  input [3:0] bgn17;
-  input [3:0] bgn18;
-  output [6:2] sz0;
-  output [6:2] sz1;
-  output [6:2] sz2;
-  output [6:2] sz3;
-  output [12:4] cnt1;
-  output [18:6] cnt2;
-  output [24:8] cnt3;
-  output [30:10] cnt4;
-
-  );
-  sz2 s0_med(
-  bgn0,
-  bgn2,
-  bgn3,
-  bgn4,
-  bgn6,
-  sz0,
-  sz1,
-  cnt1
-  );
-
-  sz2 s4_med(
-  bgn4,  bgn6,  bgn7,  bgn8,  bgn10,
-  sz0x[4],  sz1x[4],  cnt1x[4]
-  );
-  sz2 s5_med(
-  bgn5,  bgn7,  bgn8,  bgn9,  bgn11,
-  sz0x[5],  sz1x[5],  cnt1x[5]
-  );
-  sz2 s6_med(
-  bgn6,  bgn8,  bgn9,  bgn10,  bgn12,
-  sz0x[6],  sz1x[6],  cnt1x[6]
-  );
-  sz2 s7_med(
-  bgn7,  bgn9,  bgn10,  bgn11,  bgn13,
-  sz0x[7],  sz1x[7],  cnt1x[7]
-  );
-  sz2 s8_med(
-  bgn8,  bgn10,  bgn11,  bgn12,  bgn14,
-  sz0x[8],  sz1x[8],  cnt1x[8]
-  );
-  sz2 s9_med(
-  bgn9,  bgn11,  bgn12,  bgn13,  bgn15,
-  sz0x[9],  sz1x[9],  cnt1x[9]
-  );
-  sz2 s10_med(
-  bgn10,  bgn12,  bgn13,  bgn14,  bgn16,
-  sz0x[10],  sz1x[10],  cnt1x[10]
-  );
-  sz2 s11_med(
-  bgn11,  bgn13,  bgn14,  bgn15,  bgn17,
-  sz0x[11],  sz1x[11],  cnt1x[11]
-  );
-  sz2 s12_med(
-  bgn12,  bgn14,  bgn15,  bgn16,  bgn18,
-  sz0x[12],  sz1x[12],  cnt1x[12]
-  );
-
