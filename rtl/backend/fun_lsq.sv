a3f8167db4e5d49a2a611c3cb7ac32dbbbcab899
Goran Dakov <mbgoran@yahoo.com>
2022-05-21 14:01:59 +0100

    added some stuff to fun_lsq.

--- rtl/backend/fun_lsq.sv
+++ rtl/backend/fun_lsq.sv
@@ -22,8 +22,8 @@ module fun_lsq(
   st_stall,
   st0_adata,st0_en,st0_bank1,st0_bgn_ben,st0_end_ben,st0_data,st0_pbit,
   st1_adata,st1_en,st1_bank1,st1_bgn_ben,st1_end_ben,st1_data,st1_pbit,
-  wb0_adata,wb0_LSQ,wb0_en,wb0_ret,wb0_data,wb0_sdata,wb0_brdbanks,wb0_pbit,
-  wb1_adata,wb1_LSQ,wb1_en,wb1_ret,wb1_data,wb1_sdata,wb1_brdbanks,wb1_pbit,
+  wb0_adata,wb0_LSQ,wb0_en,wb0_ret,wb0_data,wb0_sdata,wb0_brdbanks,wb0_brdbanks2,wb0_pbit,
+  wb1_adata,wb1_LSQ,wb1_en,wb1_ret,wb1_data,wb1_sdata,wb1_brdbanks,wb1_brdbanks2,wb1_pbit,
   mem_II_upper,
   mem_II_upper_in,
   mem_II_bits_fine,
@@ -116,7 +116,8 @@ module fun_lsq(
   output [12:0]              wb0_ret;
   output [127+8:0]           wb0_data;
   output [127+8:0]           wb0_sdata;
-  output [3:0]               wb0_brdbanks;
+  output [16:0]               wb0_brdbanks;
+  output [16:0]               wb0_brdbanks2;
   output [1:0]               wb0_pbit;
   output [`lsaddr_width-1:0] wb1_adata;
   output [8:0]               wb1_LSQ;
@@ -124,7 +125,8 @@ module fun_lsq(
   output [12:0]              wb1_ret;
   output [127+8:0]           wb1_data;
   output [127+8:0]           wb1_sdata;
-  output [3:0]               wb1_brdbanks;
+  output [16:0]               wb1_brdbanks;
+  output [16:0]               wb1_brdbanks2;
   output [1:0]               wb1_pbit;
   
   output [5:0]   mem_II_upper;
@@ -329,8 +331,8 @@ module fun_lsq(
   lsw_wq1,lsw_rs_en1[0],lsw_wdata1,lsw_pdata1,
   LSQ_shr_data[`lsqshare_wrt0]!=3'd7,
   LSQ_shr_data[`lsqshare_wrt1]!=3'd7,
-  wb1_adata,wb1_LSQ,wb1_data,wb1_pbit,wb1_brdbanks,wb1_en,,wb1_chk,//wb1_way
-  wb0_adata,wb0_LSQ,wb0_data,wb0_pbit,wb0_brdbanks,wb0_en,wb0_chk,
+  wb1_adata,wb1_LSQ,wb1_data,wb1_pbit,wb1_brdbanks,wb1_brdbanks2,wb1_en,,wb1_chk,//wb1_way
+  wb0_adata,wb0_LSQ,wb0_data,wb0_pbit,wb0_brdbanks,wb0_brdbanks2,wb0_en,wb0_chk,
   wreq_en[0],wreq_data[0],dc_wdataP[0],dc_pdataP[0],
   wreq_en[1],wreq_data[1],dc_wdataP[1],dc_pdataP[1]
   );
