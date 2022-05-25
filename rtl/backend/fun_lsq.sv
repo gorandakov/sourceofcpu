eb8e80cb7de276621917b7996872e78f75b3ab98
Goran Dakov <mbgoran@yahoo.com>
2022-05-25 15:26:09 +0100

    adjsuted some stuff

--- rtl/backend/fun_lsq.sv
+++ rtl/backend/fun_lsq.sv
@@ -488,10 +488,10 @@ module fun_lsq(
   .rsEn0(p0_rsEn&&~p0_lsfwd),.rsEn1(p1_rsEn&&~p1_lsfwd),.rsEn2(p2_rsEn&&~p2_lsfwd),.rsEn3(p3_rsEn&&~p3_lsfwd)
   );  
   
-  wrtdata_combine wcomb0_mod(.data(dc_wdataP[0]),.pdata(dc_pdataP[0]),.en(1'b1),
-    .odata(st0_data),.opdata(st0_pbit),.low(wreq_data[0][`lsaddr_low]),.sz(wreq_data[0][`lsaddr_sz]));
-  wrtdata_combine wcomb1_mod(.data(dc_wdataP[1]),.pdata(dc_pdataP[1]),.en(1'b1),
-      .odata(st1_data),.opdata(st1_pbit),.low(wreq_data[1][`lsaddr_low]),.sz(wreq_data[1][`lsaddr_sz]));
+  wrtdata_combine wcomb0_mod(.data(dc_wdataP_reg[0]),.pdata(dc_pdataP_reg[0]),.en(1'b1),
+    .odata(st0_data),.opdata(st0_pbit),.low(wreq_data_reg[0][`lsaddr_low]),.sz(wreq_data_reg[0][`lsaddr_sz]));
+  wrtdata_combine wcomb1_mod(.data(dc_wdataP_reg[1]),.pdata(dc_pdataP_reg[1]),.en(1'b1),
+      .odata(st1_data),.opdata(st1_pbit),.low(wreq_data_reg[1][`lsaddr_low]),.sz(wreq_data_reg[1][`lsaddr_sz]));
 
   
   wire [127+8:0]           wb1_dataA;
