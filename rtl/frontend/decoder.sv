a34f872f997a6afbcbf99594f688002c453a5fb1
pixxie dust <goran.dakov@gmail.com>
2022-08-31 12:49:11 -0400

    Revert "added sine stuff."
    This reverts commit 65af54785e54d2f208761a92abecca3325f265ca.

--- rtl/frontend/decoder.sv
+++ rtl/frontend/decoder.sv
@@ -91,9 +91,9 @@ module decoder_permitted_i(
           assign permB[k]=branch_cnt[k][2] & taken_cnt[k][1] & indir_cnt[k][1];
           
           if (k>0)
-              assign permC[k]=(|(sys[k:0])) ? sys[k-1:0]==0 && pos0[k]==0  : pos0[k]==0 ;
+              assign permC[k]=(|(sys[k:0])) ? sys[k-1:0]==0 && pos0[k]==0 && FPU_dke[k][3] : pos0[k]==0 && FPU_dke[k][3];
           else
-              assign permC[k]=(pos0[0] && allret)==0 ;
+              assign permC[k]=(pos0[0] && allret)==0 && FPU_dke[k][3];
       end
   endgenerate
   
