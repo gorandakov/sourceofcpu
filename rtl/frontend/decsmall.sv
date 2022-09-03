409c3f6e223a2b9f866f7c99ed6ca74742419915
pixxie dust <goran.dakov@gmail.com>
2022-09-03 07:09:21 -0400

    added unsigned immediate alu instructions. (32 bit imm).

--- rtl/frontend/decsmall.sv
+++ rtl/frontend/decsmall.sv
@@ -904,13 +904,12 @@ module smallInstr_decoder(
                prT[9]={instr[49],instr[15:12]};
                prB[9]=5'd31;
                perror[9]=0;
-	       if (~opcode_main[0]) pconstant[9]={instr[47:16],{32{instr[16]}}};
+	       if (~opcode_main[0]) pconstant[9]={32'b0,instr[47:16]]};
            end else begin
                prA[9]={1'b0,instr[11:8]};
                prT[9]={1'b0,instr[15:12]};
                prB[9]=5'd31;
-	       if (~opcode_main[0]) pconstant[9]={instr[47:16],{32{instr[16]}}};
-	       perror[9]=1'b0;
+	       if (~opcode_main[0]) pconstant[9]={32'b0,instr[47:16]};
            end
        end else begin
            if (magic[1:0]==2'b01) begin
