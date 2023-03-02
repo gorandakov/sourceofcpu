cf5a85f1326b4f7ca165974136c6a572ed07c443
Goran Dakov <gorandakov@heptane.co.uk>
2023-03-02 08:52:36 -0500

    added some stuff to dtlb.

--- rtl/cache/dtlb.sv
+++ rtl/cache/dtlb.sv
@@ -24,8 +24,18 @@ limitations under the License.
 module dtlb_ram(
   clk,
   rst,
-  read_addr,
-  read_data,
+  read0_addr,
+  read0_data,
+  read1_addr,
+  read1_data,
+  read2_addr,
+  read2_data,
+  read3_addr,
+  read3_data,
+  read4_addr,
+  read4_data,
+  read5_addr,
+  read5_data,
   write_addr,
   write_data,
   write_wen
@@ -45,7 +55,12 @@ module dtlb_ram(
 
   reg [DATA_WIDTH-1:0] ram [ADDR_COUNT-1:0];
   
-  assign read_data=ram[read_addr];
+  assign read0_data=ram[read0_addr];
+  assign read1_data=ram[read1_addr];
+  assign read2_data=ram[read2_addr];
+  assign read3_data=ram[read3_addr];
+  assign read4_data=ram[read4_addr];
+  assign read5_data=ram[read5_addr];
 
   always @(posedge clk)
     begin
@@ -95,9 +110,9 @@ module dtlb_way(
   clk,
   rst,
   read_clkEn,
-  sec_wren,
-  addr,
   sproc,
+  R0_sec_wren,
+  addr,
   read_data0,
   read_data1,
   read_data2,
@@ -132,13 +147,13 @@ module dtlb_way(
   input clk;
   input rst;
   input read_clkEn;
-  input sec_wren;
-  input [VADDR_WIDTH-15:0] addr;
   input [20:0] sproc;
-  output [OUTDATA_WIDTH-1:0] read_data0;
-  output [OUTDATA_WIDTH-1:0] read_data1;
-  output [OUTDATA_WIDTH-1:0] read_data2;
-  output [2:0] read_way;
+  input R0_sec_wren;
+  input [5:0][VADDR_WIDTH-15:0]   addr;
+  output [5:0][OUTDATA_WIDTH-1:0] read_data0;
+  output [5:0][OUTDATA_WIDTH-1:0] read_data1;
+  output [5:0][OUTDATA_WIDTH-1:0] read_data2;
+  output [5:0][2:0]               read_way;
   output [2:0] read_lru;
   output [2:0] read_lruW;
   output read_hit;
@@ -156,11 +171,11 @@ module dtlb_way(
   input init;
   input [3:0] initCount;
   
-  wire valid;
-  wire shared;
-  wire [VADDR_WIDTH-15:0] vaddr;
+  wire valid[5:0];
+  wire shared[5:0];
+  wire [VADDR_WIDTH-15:0] vaddr[5:0];
   wire write_wen_ram;
-  wire [DATA_WIDTH-1:0] read_data_ram;
+  wire [DATA_WIDTH-1:0] read_data_ram[5:0];
   wire [DATA_WIDTH-1:0] write_data_ram;
   wire [DATA_WIDTH-1:0] write_data_new;
 
@@ -168,24 +183,29 @@ module dtlb_way(
   
   
 
-  assign vaddr=read_data_ram[`dtlb_vaddr];
-  assign valid=read_data_ram[`dtlb_valid];
-  assign shared=read_data_ram[`dtlb_shr];
+  generate
+    genvar p;
+    for(p=0;p<6;p=p+1) begin
+        assign vaddr[p]=read_data_ram[p][`dtlb_vaddr];
+        assign valid[p]=read_data_ram[p][`dtlb_valid];
+        assign shared[p]=read_data_ram[p][`dtlb_shr];
 
-  assign read_hitL=valid && (vaddr==addr || ({sproc,addr[29:0]}==vaddr && 
-    shared));
-  assign read_hit=read_hitL;
+        assign read_hitL[p]=valid[p] && (vaddr[p]==addr[p] || ({sproc,addr[p][29:0]}==vaddr[p] && 
+          shared[p]));
+        assign read_hit[p]=read_hitL[p];
   
+        assign read_data0[p]=read_hitL[p] ? read_data_ram[p][`dtlb_data1] : 'z;
+        assign read_data1[p]=read_hitL[p] ? read_data_ram[p][`dtlb_data2] : 'z;
+        assign read_data2[p]=read_hitL[p] ? read_data_ram[p][`dtlb_data3] : 'z;
+
+        assign read_way=read_hit ? WAYNO[2:0] : 3'bz;
+
+    end
+  endgenerate
   assign write_wen_ram=(write_wen && write_lru==3'd7 && ~write_xstant) 
-  || init || write_wen & write_xstant & ~force_way_off  & (read_hitL|
+  || init || write_wen & write_xstant & ~force_way_off  & (read_hitL[0]|
   force_way_on);
   
-  assign read_data0=read_hitL ? read_data_ram[`dtlb_data1] : 'z;
-  assign read_data1=read_hitL ? read_data_ram[`dtlb_data2] : 'z;
-  assign read_data2=read_hitL ? read_data_ram[`dtlb_data3] : 'z;
-
-  assign read_way=read_hit ? WAYNO[2:0] : 3'bz;
-
   assign write_data_new[`dtlb_vaddr]=write_addr;
   assign write_data_new[`dtlb_valid]=~init && ~write_invl;
   assign write_data_new[`dtlb_shr]=write_data0[`dtlbData_glo] && write_data1[
@@ -202,8 +222,18 @@ module dtlb_way(
   dtlb_ram ram_mod(
   .clk(clk),
   .rst(rst),
-  .read_addr(addr[3:0]),
-  .read_data(read_data_ram),
+  .read_addr(addr[0][3:0]),
+  .read_data(read_data_ram[0]),
+  .read_addr(addr[1][3:0]),
+  .read_data(read_data_ram[1]),
+  .read_addr(addr[2][3:0]),
+  .read_data(read_data_ram[2]),
+  .read_addr(addr[3][3:0]),
+  .read_data(read_data_ram[3]),
+  .read_addr(addr[4][3:0]),
+  .read_data(read_data_ram[4]),
+  .read_addr(addr[5][3:0]),
+  .read_data(read_data_ram[5]),
   .write_addr(init ? initCount : write_addr[3:0]),
   .write_data(write_data_ram),
   .write_wen(write_wen_ram)
@@ -212,7 +242,7 @@ module dtlb_way(
   dtlb_lru_ram ramr_mod(
   .clk(clk),
   .rst(rst),
-  .read_addr(addr[3:0]),
+  .read_addr(addr[0][3:0]),
   .read_data(read_lru),
   .write_addr(write_wen ? write_addr[3:0] : (init? initCount : addr[3:0])),
   .write_data(newLRU),
@@ -274,12 +304,12 @@ module dtlb(
   input rst;
   input read_clkEn;
   input sec_wren;
-  input [VADDR_WIDTH-14:0] addr;
+  input [5:0][VADDR_WIDTH-14:0] addr;
   input [20:0] sproc;
-  output [OUTDATA_WIDTH-1:0] read_data;
-  output [OUTDATA_WIDTH-1:0] read_data_next;
-  output [2:0] read_way;
-  output read_hit;
+  output [5:0][OUTDATA_WIDTH-1:0] read_data;
+  output [5:0][OUTDATA_WIDTH-1:0] read_data_next;
+  output [5:0][2:0] read_way;
+  output [5:0]read_hit;
   //output [1:0] read_2M;
   input [VADDR_WIDTH-15:0] write_addr;
   input [OUTDATA_WIDTH-1:0] write_data0;
@@ -297,26 +327,32 @@ module dtlb(
   wire [7:0][2:0] lru_wayW;
   wire [2:0] hitLruW;
 
-  wire [7:0] read_hit_way;
-  wire [7:0] read_hitL_way;
+  wire [5:0][7:0] read_hit_way;
+  wire [5:0][7:0] read_hitL_way;
 
-  wire [2:0] read_way_X;
+  wire [5:0][2:0] read_way_X;
 
   assign read_way=read_way_X;
   
-  wire [OUTDATA_WIDTH-1:0] read_data0;
-  wire [OUTDATA_WIDTH-1:0] read_data1;
-  wire [OUTDATA_WIDTH-1:0] read_data2;
+  wire [5:0][OUTDATA_WIDTH-1:0] read_data0;
+  wire [5:0][OUTDATA_WIDTH-1:0] read_data1;
+  wire [5:0][OUTDATA_WIDTH-1:0] read_data2;
 
   
-  wire read_hitL,read_hitH;
+  wire [5:0]read_hitL,read_hitH;
 
   wire [3:0] wrAddrNxt;
   reg [3:0] initCount;
   reg init;
   
   genvar k;
-  
+  wire [5:0][VADDR_WIDTH-15:0] addr0;
+  assign addr0[0]=addr[0][VADDR_WIDTH-14:1];
+  assign addr0[1]=addr[1][VADDR_WIDTH-14:1];
+  assign addr0[2]=addr[2][VADDR_WIDTH-14:1];
+  assign addr0[3]=addr[3][VADDR_WIDTH-14:1];
+  assign addr0[4]=addr[4][VADDR_WIDTH-14:1];
+  assign addr0[5]=addr[5][VADDR_WIDTH-14:1];
   generate
     for(k=0;k<WAY_COUNT;k=k+1)
 	  begin : ways_gen
@@ -325,7 +361,7 @@ module dtlb(
         .rst(rst),
         .read_clkEn(read_clkEn),
 	.sec_wren(sec_wren),
-        .addr(addr[VADDR_WIDTH-14:1]),
+        .addr(addr0),
         .sproc(sproc[20:0]),
         .read_data0(read_data0),
         .read_data1(read_data1),
@@ -357,29 +393,74 @@ module dtlb(
         .en(read_clkEn|write_wen)
         );
 
-	assign hitLru=read_hitL_way[k] ? lru_way[k] : 3'bz;
+	assign hitLru=read_hitL_way[0][k] ? lru_way[k] : 3'bz;
 	assign hitLruW=3'd7;
 	
       end
   endgenerate
   
-  assign hitLru=read_hitL ? 3'bz : 3'b0;
+  assign hitLru=read_hitL[0] ? 3'bz : 3'b0;
   
-  assign read_hitL=|read_hit_way;
+  assign read_hitL[0]=|read_hit_way[0];
+  assign read_hitL[1]=|read_hit_way[1];
+  assign read_hitL[2]=|read_hit_way[2];
+  assign read_hitL[3]=|read_hit_way[3];
+  assign read_hitL[4]=|read_hit_way[4];
+  assign read_hitL[5]=|read_hit_way[5];
 
-  assign read_hit=read_hitL && ~init;
+  assign read_hit[0]=read_hitL[0] && ~init;
+  assign read_hit[1]=read_hitL[1] && ~init;
+  assign read_hit[2]=read_hitL[2] && ~init;
+  assign read_hit[3]=read_hitL[3] && ~init;
+  assign read_hit[4]=read_hitL[4] && ~init;
+  assign read_hit[5]=read_hitL[5] && ~init;
 //  assign read_2M[0]=read_data[`dtlbData_subpage];;
 //  assign read_2M[1]=read_data_next[`dtlbData_subpage];;
   
-  assign read_data0=read_hit ? 'z : {OUTDATA_WIDTH{1'B0}};
-  assign read_data1=read_hit ? 'z : {OUTDATA_WIDTH{1'B0}};
-  assign read_data2=read_hit ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data0[0]=read_hit[0] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data1[0]=read_hit[0] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data2[0]=read_hit[0] ? 'z : {OUTDATA_WIDTH{1'B0}};
 
-  assign read_way_X=read_hit ? 3'BZ : 3'B0;
+  assign read_data0[1]=read_hit[1] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data1[1]=read_hit[1] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data2[1]=read_hit[1] ? 'z : {OUTDATA_WIDTH{1'B0}};
+
+  assign read_data0[2]=read_hit[2] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data1[2]=read_hit[2] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data2[2]=read_hit[2] ? 'z : {OUTDATA_WIDTH{1'B0}};
+
+  assign read_data0[3]=read_hit[3] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data1[3]=read_hit[3] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data2[3]=read_hit[3] ? 'z : {OUTDATA_WIDTH{1'B0}};
+
+  assign read_data0[4]=read_hit[4] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data1[4]=read_hit[4] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data2[4]=read_hit[4] ? 'z : {OUTDATA_WIDTH{1'B0}};
+
+  assign read_data0[5]=read_hit[5] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data1[5]=read_hit[5] ? 'z : {OUTDATA_WIDTH{1'B0}};
+  assign read_data2[5]=read_hit[5] ? 'z : {OUTDATA_WIDTH{1'B0}};
+
+  assign read_way_X[0]=read_hit[0] ? 3'BZ : 3'B0;
+  assign read_way_X[1]=read_hit[1] ? 3'BZ : 3'B0;
+  assign read_way_X[2]=read_hit[2] ? 3'BZ : 3'B0;
+  assign read_way_X[3]=read_hit[3] ? 3'BZ : 3'B0;
+  assign read_way_X[4]=read_hit[4] ? 3'BZ : 3'B0;
+  assign read_way_X[5]=read_hit[5] ? 3'BZ : 3'B0;
   
-  assign read_data=(~addr[0]) ? read_data0 : read_data1;
+  assign read_data[0]=(~addr[0][0]) ? read_data0[0] : read_data1[0];
+  assign read_data[1]=(~addr[1][0]) ? read_data0[1] : read_data1[1];
+  assign read_data[2]=(~addr[2][0]) ? read_data0[2] : read_data1[2];
+  assign read_data[3]=(~addr[3][0]) ? read_data0[3] : read_data1[3];
+  assign read_data[4]=(~addr[4][0]) ? read_data0[4] : read_data1[4];
+  assign read_data[5]=(~addr[5][0]) ? read_data0[5] : read_data1[5];
   
-  assign read_data_next=(~addr[0]) ? read_data1 : read_data2;
+  assign read_data_next[0]=(~addr[0][0]) ? read_data1[0] : read_data2[0];
+  assign read_data_next[1]=(~addr[1][0]) ? read_data1[1] : read_data2[0];
+  assign read_data_next[2]=(~addr[2][0]) ? read_data1[2] : read_data2[0];
+  assign read_data_next[3]=(~addr[3][0]) ? read_data1[3] : read_data2[0];
+  assign read_data_next[4]=(~addr[4][0]) ? read_data1[4] : read_data2[0];
+  assign read_data_next[5]=(~addr[5][0]) ? read_data1[5] : read_data2[0];
   
   adder_inc #(4) initAdd_mod(initCount,wrAddrNxt,1'b1,);
   
