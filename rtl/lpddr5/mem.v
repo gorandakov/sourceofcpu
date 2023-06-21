f9fff34590ff3c312224591e74d65be333d5bb36
Goran Dakov <gorandakov@heptane.co.uk>
2023-06-21 13:00:16 -0400

    added some stuff to mem.v

---
+++ rtl/lpddr5/mem.v
@@ -0,0 +1,21 @@
+
+module lpddr5_channel(
+  clk,
+  rst,
+  read_clkEn,
+  read_addr,
+  read_req,
+  read_busID,
+  readOut_addr,
+  readOut_req,
+  readOut_busID,
+  readOut_en,
+  mem_clk,
+  RAS,
+  CAS,
+  CS0,
+  ADDR15,
+  DATA18);
+  parameter ODD_HALF=0;  
+  
+endmodule  
