1fccc026d38a682d66dbcacd9789cd9226ea1186
Goran Dakov <gorandakov@heptane.co.uk>
2023-07-06 14:39:44 -0400

    added some stuff to cntrl.v

--- rtl/cntrl/baal.v
+++ rtl/cntrl/baal.v
@@ -806,9 +806,9 @@ module cntrl_find_outcome(
           assign ret_prevV[j][k]=ret_prev[j][k] && isVec[j] && isVec[k];
           assign ret_prevF[j][k]=ret_prev[j][k] && isFPU[j] && isFPU[k];
           
-	  assign tireG[j]=reteq ? ~xbreak[k]  && ret_prevG[k]==0 && isGen[k] && ~rT[k][5] : 1'bz; 
-          assign tireV[j]=reteq ? ~xbreak[k]  && ret_prevV[k]==0 && isVec[k] && ~rT[k][5]  : 1'bz; 
-          assign tireF[j]=reteq ? ~xbreak[k]  && ret_prevF[k]==0 && isFPU[k] && ~rT[k][5]  : 1'bz;
+	  assign tireG[j]=reteq ? ~xbreak[k]  && ret_prevG[k]==0 && isGen[k] && ~&rT[k] : 1'bz; 
+          assign tireV[j]=reteq ? ~xbreak[k]  && ret_prevV[k]==0 && isVec[k] && ~&rT[k]  : 1'bz; 
+          assign tireF[j]=reteq ? ~xbreak[k]  && ret_prevF[k]==0 && isFPU[k] && ~&rT[k]  : 1'bz;
 
           assign rTe[j]=reteq ? rT[k] : 6'bz;
 
