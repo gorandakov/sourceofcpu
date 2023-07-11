19d0370280af03855565920961ec81458ad9fd23
Goran Dakov <gorandakov@heptane.co.uk>
2023-07-11 06:18:06 -0400

    added some stuff to icompr.v

--- rtl/math/icompr.v
+++ rtl/math/icompr.v
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-module icompr(clk,clkEn,R,C,A_out,B_out,and1,is_sig,sig,sm_sig);
+(* align_width=C,A_out/2,B_out/2 align_height=R *) module icompr(clk,clkEn,R,C,A_out,B_out,and1,is_sig,sig,sm_sig);
   input clk;
   input clkEn;
   input [63:0] R;
