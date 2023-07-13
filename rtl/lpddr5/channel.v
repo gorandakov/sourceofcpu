0036811f840971159bb161798c69831b75ef984e
Goran Dakov <gorandakov@heptane.co.uk>
2023-07-13 09:31:09 -0400

    added some stuff to lpddr5.

--- rtl/lpddr5/channel.v
+++ rtl/lpddr5/channel.v
@@ -59,8 +59,72 @@ module lpddr5_tag_ram(
 
 endmodule
 
+module lpddr5_queue_ram(
+  clk,
+  rst,
+  read_clkEn,
+  read_addr,
+  read_data,
+  write_addr,
+  write_data,
+  write_wen);
+  input clk;
+  input rst;
+  input read_clkEn;
+  input [3:0] read_addr;
+  output [`ramQ_width-1:0] read_data;
+  input [3:0] write_addr;
+  input [`ramQ_width-1:0] write_data;
+  input write_wen;
 
-module lpddr5_cache_way(
+  reg [`ramQ_width-1:0] RAM[15:0];
+
+  assign read_data=RAM[read_addr_reg];
+
+  always @(posedge clk) begin
+      if (rst) read_addr_reg<=0;
+      else if (read_clkEn) read_addr_reg<=read_addr;
+      if (write_wen) RAM[write_addr]<=write_data;
+  end
+
+endmodule
+module lpddr5_Rqueue(
+  clk,
+  rst,
+  doStall,
+  read_clkEn,
+  read_addr,
+  read_data,
+  write_addr,
+  write_data,
+  write_wen);
+  input clk;
+  input rst;
+  output reg doStall;
+  input read_clkEn;
+  input [3:0] read_addr;
+  output [`ramQ_width-1:0] read_data;
+  input [3:0] write_addr;
+  input [`ramQ_width-1:0] write_data;
+  input write_wen;
+  
+  reg [3:0] cur_pos;
+  reg [4:0] cnt;
+
+  always @(posedge clk) begin
+      if (rst) begin
+          cur_pos<=0;
+          cnt<=0;
+          doStall<=0;
+      end else begin
+          if (read_clkEn) cur_pos<=cur_pos_inc;
+          if (read_clkEn & ~ write_wen) cnt<=cnt_dec;
+          if (~read_clkEn & write_wen) cnt<=cnt_inc;
+          doStall<=cnt==15 || cnt==16;
+      end
+  end
+endmodule
+module lpddr5_cache(
   clk,
   rst,
   reqRD_en,reqRD_req,reqRD_dirty,
@@ -77,6 +141,4 @@ module lpddr5_cache_way(
 
 endmodule
 
-module lpddr5_cache();
-endmodule
 
