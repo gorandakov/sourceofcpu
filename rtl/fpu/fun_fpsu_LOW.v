bdba3f9b0e84c5a64fafa76a4f6908bf48a2ec5c
Goran Dakov <gorandakov@heptane.co.uk>
2023-07-20 06:40:51 -0400

    added some stuff to fp{CS}u.v

--- rtl/fpu/fun_fpsu_LOW.v
+++ rtl/fpu/fun_fpsu_LOW.v
@@ -24,13 +24,13 @@ module fun_fpuSL(
   fpcsr,
   u1_A,u1_B,u1_Bx,u1_Bxo,u1_en,u1_op,
   u1_fufwd_A,u1_fuufwd_A,u1_fufwd_B,u1_fuufwd_B,
-  u1_ret,u1_ret_en,u1_XSUB,
+  u1_ret,u1_ret_en,u1_XSUB,u1_FX,
   u3_A,u3_B,u3_Bx,u3_Bxo,u3_en,u3_op,
   u3_fufwd_A,u3_fuufwd_A,u3_fufwd_B,u3_fuufwd_B,
-  u3_ret,u3_ret_en,u3_XSUB,
+  u3_ret,u3_ret_en,u3_XSUB,u3_FX,
   u5_A,u5_B,u5_Bx,u5_Bxo,u5_en,u5_op,
   u5_fufwd_A,u5_fuufwd_A,u5_fufwd_B,u5_fuufwd_B,
-  u5_ret,u5_ret_en,u5_XSUB,
+  u5_ret,u5_ret_en,u5_XSUB,u5_FX,
   FUF0,FUF1,FUF2,
   FUF3,FUF4,FUF5,
   FUF6,FUF7,FUF8,
@@ -64,6 +64,7 @@ module fun_fpuSL(
   output [13:0] u1_ret;
   output u1_ret_en;
   input u1_XSUB;
+  input u1_FX;
 
   input [67:0] u3_A;
   input [67:0] u3_B;
@@ -78,6 +79,7 @@ module fun_fpuSL(
   output [13:0] u3_ret;
   output u3_ret_en;
   input u3_XSUB;
+  input u3_FX;
   
   input [67:0] u5_A;
   input [67:0] u5_B;
@@ -92,6 +94,7 @@ module fun_fpuSL(
   output [13:0] u5_ret;
   output u5_ret_en;
   input u5_XSUB;
+  input u5_FX;
   
 
   (* register equiload *) input [67:0] FUF0;
@@ -133,7 +136,7 @@ module fun_fpuSL(
   fpcsr,
   u1_A,u1_B,u1_Bx,u1_Bxo,u1_en,u1_op,
   u1_fufwd_A,u1_fuufwd_A,u1_fufwd_B,u1_fuufwd_B,
-  u1_ret,u1_ret_en,u1_XSUB,
+  u1_ret,u1_ret_en,u1_XSUB,u1_FX,
   FUF0,FUF1,FUF2,
   FUF3,FUF4,FUF5,
   FUF6,FUF7,FUF8,
@@ -151,7 +154,7 @@ module fun_fpuSL(
   fpcsr,
   u3_A,u3_B,u3_Bx,u3_Bxo,u3_en,u3_op,
   u3_fufwd_A,u3_fuufwd_A,u3_fufwd_B,u3_fuufwd_B,
-  u3_ret,u3_ret_en,u3_XSUB,
+  u3_ret,u3_ret_en,u3_XSUB,u3_FX,
   FUF0,FUF1,FUF2,
   FUF3,FUF4,FUF5,
   FUF6,FUF7,FUF8,
@@ -169,7 +172,7 @@ module fun_fpuSL(
   fpcsr,
   u5_A,u5_B,u5_Bx,u5_Bxo,u5_en,u5_op,
   u5_fufwd_A,u5_fuufwd_A,u5_fufwd_B,u5_fuufwd_B,
-  u5_ret,u5_ret_en,u5_XSUB,
+  u5_ret,u5_ret_en,u5_XSUB,u5_FX,
   FUF0,FUF1,FUF2,
   FUF3,FUF4,FUF5,
   FUF6,FUF7,FUF8,
