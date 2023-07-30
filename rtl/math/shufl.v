833c2529bfc9c97a0cbcd4156b3b0fe7690d1b31
Goran Dakov <gorandakov@heptane.co.uk>
2023-07-30 14:09:21 +0100

    added some stuff to shufl.v. sqrt/div prepo

--- rtl/math/shufl.v
+++ rtl/math/shufl.v
@@ -59,14 +59,18 @@ module fperm(
   
   adder #(12) add_dbl(BIAS_D,~B[65:54],exp_D,1'b1,is_sqrt,,,,);
   adder #(12) add_dbl(BIAS_D,~{1'b0,B[65:55]},exp_D,1'b1,~is_sqrt,,,,);
+  adder #(9) add_dbl(BIAS_S,~B[65:57],exp_X,1'b1,is_sqrt,,,,);
+  adder #(9) add_dbl(BIAS_S,~{1'b0,B[65:58]},exp_X,1'b1,~is_sqrt,,,,);
+  adder #(9) add_dbl(BIAS_S,~B[32:24],exp_X1,1'b1,is_sqrt,,,,);
+  adder #(9) add_dbl(BIAS_S,~{1'b0,B[32:25]},exp_X1,1'b1,~is_sqrt,,,,);
 
-  assign resY=A[67:66]==`ftype_dbl ? {B[67:66],exp_D,B[53],53'b0} : 68'bz;
+  assign resY=A[67:66]==`ftype_dbl ? {B[67:66],exp_D,B[53],53'b0} : {B[67:66],exp_X,B[56],23'b0,exp_X1,B[23],23'b0};
 
   assign resX=(copyA & ~swpSngl) ? A : 68'bz;
   assign resX=(~copyA & ~swpSngl) ? B : 68'bz;
   assign resX=(copyA & swpSngl) ? {A[67:66],A[32:0],A[65:33]} : 68'bz;
   assign resX=(~copyA & swpSngl) ? {B[67:66],B[32:0],B[65:33]} : 68'bz;
-  assign res0=dupSngl ?{resX[67:66],resX[32:0],resX[32:0]} : resX; 
+  assign res0=dupSngl ?{resX[67:66],resX[32:0],resX[32:0]} : is_sqrt|is_div ? resY : resX; 
   always @(negedge clk) begin
     res0_reg<=res0;
     res0_reg2<=res0_reg;
