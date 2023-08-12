e860d06861724829740d7a0b5ab9a8b913faefa2
Goran Dakov <gorandakov@heptane.co.uk>
2023-08-12 14:22:44 +0100

    added some stuff to tbl.v

--- rtl/math/tbl.v
+++ rtl/math/tbl.v
@@ -29,12 +29,12 @@ module tblD(
       bits=6'd0;
       if (xtra==2'd0) bits= A[53:48] : 6'bz;
       if (xtra==2'd1) bits= {A[54],A[53:49]} : 6'bz;
-      if (xtra[1] && A[65:54]==2044) bits= {5'b0,A[53]} : 6'bz;  
-      if (xtra[1] && A[65:54]==2045) bits= {4'b0,A[53:52]} : 6'bz;  
-      if (xtra[1] && A[65:54]==2046) bits= {3'b0,A[53:51]} : 6'bz;  
-      if (xtra[1] && A[65:54]==2047) bits= {2'b0,A[53:50]} : 6'bz;  
-      if (xtra[1] && A[65:54]==2048) bits= {1'b0,A[53:49]} : 6'bz;  
-      if (xtra[1] && A[65:54]==2049) bits= A[53:48] : 6'bz;
+      if (xtra[1] && A[65:54]==2045) bits= {5'b0,A[53]} : 6'bz;  
+      if (xtra[1] && A[65:54]==2046) bits= {4'b0,A[53:52]} : 6'bz;  
+      if (xtra[1] && A[65:54]==2047) bits= {3'b0,A[53:51]} : 6'bz;  
+      if (xtra[1] && A[65:54]==2048) bits= {2'b0,A[53:50]} : 6'bz;  
+      if (xtra[1] && A[65:54]==2049) bits= {1'b0,A[53:49]} : 6'bz;  
+      if (xtra[1] && A[65:54]==2050) bits= A[53:48] : 6'bz;
   end  
 
   always @(posedge clk) begin
