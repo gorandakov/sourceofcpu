db91e9188d976211470c34d775027e7c6d9267c3
Goran Dakov <gorandakov@heptane.co.uk>
2023-08-22 10:48:29 +0100

    added some stuffe.

--- rtl_test/r_core_mem/selftest.v
+++ rtl_test/r_core_mem/selftest.v
@@ -23,7 +23,7 @@ module testcase(
   reg [4*33-1:0] RAM_RETIRE[1024*1024*8-1:0]
   reg [4*33-1:0] reti_read_data[9:0];
   reg [22:0] retire_index;
-  integer k,j;
+  integer k,j,p,n,err,noretcnt;
 
   reg [4:0] rA[9:0];
   reg [4:0] rB[9:0];
@@ -72,6 +72,35 @@ module testcase(
 
   always @(posedge clk) begin
       ret_enX<=ret_enX0;
+      err=-1;
+      if (rst) noretcnt=0;
+      if (!rst) begin
+          for(p=0;p<10;p=p+1) begin
+              if (read_enX[p] && ~read_enX[(p+1)%10]|(p==9)) begin
+                  noretcnt=0;
+              end
+              if (pook_en[p]) begin
+                  for(n=0;n<9;n=n+1) begin
+                      if ((ret_rT[n]==rT[p] && ret_data[n]=data[p])||!rT_en[p]) begin
+                          err=p;
+                      end else if (ret_rT[n]==rT[p] && ret_en[n] && rT_en[p] && pook_en[p]) begin
+                          $display("errval velrilog=%x, c++=%x",ret_data[n],data[p]);
+                      end                     
+                  end
+                  if (err!=p) $display("miss-execution %%RIP=0x%x, rT=%i, rB=%i, rA=%i, rT_en=%i",
+                      binoff[p],rT[p],rB[p],rA[p],rT_en[p]);
+              end
+              if (read_enX[p] && ~read_enX[(p+1)%10]|(p==9)) begin
+                  retire_index=retire_index+p;
+                  if (p!=err) $finish();
+              end
+              if (read_enX==0) noretcnt=noretcnt+1;
+              if (noretcnt>2000) begin
+                  $display("error: 2000 cycles no retire");
+                  $finish();
+              end
+          end
+      end
   end
 
  
