f61f10bea922c67b0630386c85f1c4d603209958
Goran Dakov <gorandakov@heptane.co.uk>
2023-08-24 12:57:51 +0100

    added some stuff to event cache.

--- rtl/eventcache/event_inc.v
+++ rtl/eventcache/event_inc.v
@@ -3,6 +3,6 @@
 `define event_strenght 55:50
 `define event_width 56
 
-`define cache_addr 28:0
-`define cache_next_in_neuron 49:29
-`define cache_accum 63:50
+`define cache_addr 29:0
+`define cache_next_in_neuron 50:30
+`define cache_accum 63:51
