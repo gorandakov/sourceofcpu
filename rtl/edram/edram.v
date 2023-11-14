a97a8470f9ac8a043e255ab3a1b02163db6ce783
Goran Dakov <gorandakov@heptane.co.uk>
2023-11-14 10:56:01 +0000

    added some stuffe

--- rtl/edram/edram.v
+++ rtl/edram/edram.v
@@ -25,21 +25,32 @@ module edram(
   input [4:0] port_write_addr;
   input [324:0] port_write_data;
   input port_wen_plus3;
-  input port_data_plus5;
+  input port_data;
 
   reg [31:0] edram[324:0];
 
   reg [324:0] data1;
+  reg [324:0] data2;
+  wire [324:0] data3;
 
-  block_ecc_decode(
+  block_ecc_decode corr0_mod(
   data1,
   324'b0,
   ,
   xore1);
+  block_ecc_correct corr_mod( 
+  data2,
+  xore1_reg,
+  ,
+  data3);
 
   always @(posedge clk) begin
       if (port_en | port_ref_plus2) data1<=edram[port_addr_reg];
       else if (port_wen_plus3 | port_ref_plus5) edram[port_write_addr]<=data3;
+      if (port_en && (port_read_addr_reg2!=port_read_addr)|~port_ref_plus2_reg2) begin
+          data1<=data2;
+      end
+      if (port_data) data1<=port_write_data;
       xore1_reg<=xore;
       data2<=data1;
       port_ref_plus3<=port_ref_plus2 && (port_read_addr_reg2!=port_read_addr)|~port_en_reg2;
