3d5336f769696b47c53bdaac65eda2a8bf40e866
Goran Dakov Dakov <goran.dakov@gmail.com>
2024-01-29 17:24:27 +0000

    added some stuffe.

--- rtl/decoder/decoder.v
+++ rtl/decoder/decoder.v
@@ -2330,9 +2330,10 @@ module decoder(
 
   function [5:0] ffx;
     input thr;
+    input wr_gen_purp;
     input [5:0] reeg;
     begin
-        ffx=reeg[5:2]==reeg[5:2]==4'd4 ? {3'd4,reeg[1:0],thr} : { 1'b0,thr,reeg[3:0] }; 
+        ffx=wr_gen_purp && reeg[4:0]==5'h1f ? 5'h1f : { thr,reeg[4:0] }; 
     end
   endfunction
   wire [9:0] csrss_retIP_en;
@@ -3800,22 +3801,22 @@ module decoder(
           for (n=0;n<10;n=n+1) begin
               dec_operation_reg[n]<=dec_operation[n];
 	      dec_alucond_reg[n]<=dec_alucond[n];
-              dec_rA_reg[n]<=ffx(thread,dec_rA[n]);
+              dec_rA_reg[n]<=ffx(thread,1'b0,dec_rA[n]);
               dec_rndmode_reg[n]<=dec_rndmode;
               dec_rA_use_reg[n]<=dec_rA_use[n]&& dec_rA[n]!=31 && iUsed[n];
               dec_rA_useF_reg[n]<=dec_rA_useF[n] && iUsed[n];
-              dec_rB_reg[n]<=ffx(thread,dec_rB[n]);
+              dec_rB_reg[n]<=ffx(thread,1'b0,dec_rB[n]);
               dec_rB_use_reg[n]<=dec_rB_use[n] && dec_rB[n]!=31 && iUsed[n];
               dec_rB_useF_reg[n]<=dec_rB_useF[n] && iUsed[n];
               dec_useBConst_reg[n]<=dec_useBConst[n] && iUsed[n];
               dec_useAConst_reg[n]<=dec_useBConst[n] && iUsed[n] && dec_IPRel[n] && dec_rA_use[n];
-              dec_rC_reg[n]<=ffx(thread,dec_rC[n]);
+              dec_rC_reg[n]<=ffx(thread,1'b0,dec_rC[n]);
               dec_rC_use_reg[n]<=dec_rC_use[n] && dec_rC[n]!=31 && iUsed[n];
               dec_rC_useF_reg[n]<=dec_rC_useF[n] && iUsed[n];
               dec_useCRet_reg[n]<=dec_useCRet[n] && iUsed[n];
               dec_constant_reg[n]<=dec_constant[n];
               dec_constantN_reg[n]<=dec_constantN[n];
-              dec_rT_reg[n]<=ffx(thread,dec_rT[n]);
+              dec_rT_reg[n]<=ffx(thread,dec_rT_use,dec_rT[n]);
               dec_rT_use_reg[n]<=dec_rT_use[n] && iUsed[n] && ~except;
               dec_rT_useF_reg[n]<=dec_rT_useF[n] && iUsed[n] && ~except;
               dec_port_reg[n]<=dec_port[n];
