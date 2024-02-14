518bca60cc4cb8d747a3ae4e8a1abbda11ebf933
Goran Dakov <gorandakov@heptane.co.uk>
2024-02-14 16:54:38 +0000

    added some stuffe.

--- src/kernel_intf/kmembuf.h
+++ src/kernel_intf/kmembuf.h
@@ -9,4 +9,21 @@ typedef struct _kmembuf {
   long int count;
   long int min_est_size;
   struct kmembuf_entry item[];  
+  _kmembuf() {
+  }
 } *pkmembuf;
+
+template<type T> class sys_ptr {
+  void *val;
+  public:
+  T* operator () {
+      return (T*) val;
+  }
+  void operator =(T* ptr) {
+      val=ptr;
+  }
+  sys_ptr<T>() {
+  }
+};
+
+
