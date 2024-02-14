09ef24100ec8e84226e1faba854de0cb8f75f30a
Goran Dakov <gorandakov@heptane.co.uk>
2024-02-14 16:23:54 +0000

    added some stuffe.

--- src/kernel_intf/sysmsg.h
+++ src/kernel_intf/sysmsg.h
@@ -8,6 +8,16 @@
 #define pkm_srv_nest_message 0x100
 #define pkm_srv_impersonate_caller 0x200
 
+#define obj_srv_dispatcher 0x10000000
+#define obj_vfs_dispatcher 0x20000000
+#define obj_mman_dispatcher 0x30000000
+#define obj_socket_dispatcher 0x40000000
+#define obj_process_thread_dispatcher 0x50000000
+#define obj_sysctl_dispatcher 0x60000000
 
 void sysmsg_do_call( int flags, int object, pkmembuf msg, pkmembuf snd_data,
   pkmembuf continuation_data optional, pkmembuf reply_data optional);
+
+typedef (pkmembuf) *sysmsg_callback( unsigned long long flags_object, pkmembuf msg, pkmembuf snd_data,
+  pkmembuf continuation_data optional, pkmembuf reply_data optional, pkmembuf old_continuation_data);
+//returns server continuation buffer if needed
