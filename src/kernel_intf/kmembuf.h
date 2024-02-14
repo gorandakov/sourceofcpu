09a5779a15a090a0c0ba3a597f4b24253d2a5283
Goran Dakov <gorandakov@heptane.co.uk>
2024-02-14 17:17:10 +0000

    added some stuffe.

--- src/kernel_intf/kmembuf.h
+++ src/kernel_intf/kmembuf.h
@@ -5,13 +5,19 @@ typedef struct kmembuf_entry {
   void *data;
 };
 
-template<int cnt> struct _kmembuf {
+template struct _kmembuf {
   long int count;
   long int min_est_size;
+};
+
+template<int cnt> struct kmembuf : _kmembuf {
   struct kmembuf_entry item[cnt];  
   _kmembuf() {
+      count=cnt;
   }
-} *pkmembuf;
+};
+
+ 
 
 template<type T> class sys_ptr {
   void *val;
@@ -24,6 +30,27 @@ template<type T> class sys_ptr {
   }
   sys_ptr<T>() {
   }
+  set_range(int k) {
+    __asm("movq val,%r8\n"
+          "movq k, %r9\n"
+          "ptrbnd %r9,%r8,%r8\n"
+          "movq %r8,val\n");
+  }
 };
 
+template<type T> class sys_ptr_kernel {
+  void *val;
+  public:
+  T* operator () {
+      return (T*) val;
+  }
+  void operator =(T* ptr) {
+      val=ptr;
+  }
+  sys_ptr<T>() {
+  }
+};
+
+
+typedef sys_ptr_kernel<_kmembuf> pkmembuf;
 
