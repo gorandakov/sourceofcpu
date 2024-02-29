b7ca06ec60b7e32093a3f69091d8e2552dcc89cf
Goran Dakov <gorandakov@gmail.com>
2024-02-29 16:53:16 +0000

    added some stuffe.

--- util/kirnel-kernel/krnl/syscall.cpp
+++ util/kirnel-kernel/krnl/syscall.cpp
@@ -1,4 +1,16 @@
+/*
+     This program is free software: you can redistribute it and/or modify it under 
+     the terms of the GNU General Public License as published by the Free Software Foundation, 
+     version 2 of the License.
 
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program. 
+    If not, see <https://www.gnu.org/licenses/>. 
+
+*/
 #include "continuation.hpp"
 
 int syscall(void *retcap,cap_and_flags capfl,buf_and_arg strarg,buf_and_arg 
