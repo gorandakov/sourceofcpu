3146df09904e01265c306e2729ff699119e84b16
Goran Dakov <gorandakov@gmail.com>
2024-03-13 17:57:55 +0000

    added some stuff to exception handler in translaores library. citer jump.

--- util/translaores/exception_handler.cxx
+++ util/translaores/exception_handler.cxx
@@ -37,7 +37,9 @@ int exception_handler( hcont *context ) {
 #ifdef use_insn_not_in_spec
         case OP_CLOOP_A:
         case OP_CLOOP_B:
-          off=((signed short) (insn[3+xoffA*2]&0xf)<<12));
+        case OP_CITER_A:
+        case OP_CITER_B:
+          off=((signed short) (insn[3+xoffA*2]&0x7)<<12));
           off|=insn[2+xoffA*2]<<4;
           off|=(insn[1]&0xe0)>>4;
         break;
