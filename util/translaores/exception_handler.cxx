1c9b17c206c2b45f4e749884452f869ff1be2af7
Goran Dakov <gorandakov@gmail.com>
2024-03-13 16:52:29 +0000

    added some stuf to translaores library.

--- util/translaores/exception_handler.cxx
+++ util/translaores/exception_handler.cxx
@@ -1,7 +1,8 @@
 #include "main.h"
 
-extern (C) {
 transl_context *tr;
+
+extern "C" {
 
 
 int exception_handler( hcont *context ) {
@@ -16,9 +17,35 @@ int exception_handler( hcont *context ) {
       exit(0);
   } else  if (((stop_bits >> pos >>1)&1) && pos!=14) {
       switch(insn[0]) {
-        case OP_CMOV:
+        case OP_CMP_JMP:
           off=(unsigned long) (signed char) insn[3];
-          off<<=1;
+          off<<=2;
+          off|=(insn[2]&0x80)>>6;
+          break;
+        case OP_UCJUMP:
+          off=((unsigned long) (signed char) insn[3])<<17;
+          off|=insn[2]<<9;
+          off|=insn[1]<<1;
+          break;
+        case OP_CJUMP:
+          off=(unsigned long) (signed char) insn[3];
+          off<<=3;
+          off|=(insn[2]&0xc0)>>5;
+          break;
+        default:
+          exit(0);
+  } else  if (((stop_bits >> pos >>2)&1) && pos!=13) {
+      switch(insn[0]) {
+        case OP_UCJUMP:
+        case OP_CJUMP:
+          off=((unsigned long) (signed char) insn[5])<<24;
+          off|=insn[4]<<16;
+          off|=insn[3]<<8;
+          off|=insn[2]&0xfe;
+          break;
+        default:
+          exit(0);
+          
       }
   }
   void *pgoff=(char *) IP-(char *) tr->transl_region;
@@ -43,5 +70,6 @@ int exception_handler( hcont *context ) {
     //re-translate source page
     tr->translate_page(IP);
   }
+  return 0;
 }
 }
