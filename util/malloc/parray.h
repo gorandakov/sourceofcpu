3e7d69ba6a90ee1746435a5ceed13eb1f600c431
Goran Dakov <gorandakov@heptane.co.uk>
2024-04-17 16:36:57 +0100

    added some stuff to malloc library.

---
+++ util/malloc/parray.h
@@ -0,0 +1,20 @@
+
+class parray_hidden {
+  long size;
+  void *element;
+};
+
+template <class T, struct __HEAP *h> class parray : parray_hidden {
+  parray() {
+      size=0;
+      element=0;
+  }
+  parray (long _size) {
+    size=_size;
+    element=h.malloc(size*sizeof T);
+    if (!element) size=0;
+  }
+  ~parray() {
+    if (element) free(element);
+  }
+};
---
+++ util/malloc/pstring.h
@@ -0,0 +1,23 @@
+
+class pstring_hidden {
+  int size;
+  int capacity;
+  void *element;
+};
+
+template <class T=char, struct __HEAP *h> class pstring : pstring_hidden {
+  pstring() {
+      size=0;
+      capacity=0;
+      element=0;
+  }
+  pstring (long _size) {
+    size=0;
+    capacity=_size;
+    element=h.malloc((size+1)*sizeof T);
+    if (!element) capacity=0;
+  }
+  ~pstring() {
+    if (element) free(element);
+  }
+};
