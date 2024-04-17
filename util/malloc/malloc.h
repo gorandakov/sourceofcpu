1a8238b63558d03b5e3938ab7d11c20687d1c33e
Goran Dakov <gorandakov@heptane.co.uk>
2024-04-17 16:50:53 +0100

    added some stuff to malloc.

--- util/malloc/malloc.h
+++ util/malloc/malloc.h
@@ -1,7 +1,8 @@
 
 struct __HEAP {
   void * (*malloc)(struct __HEAP *h,long size);
+  void * (*mrealloc)(struct __HEAP *h,long size_new);
   unsigned long module_handle;
   void *first_block;
-  void *helper; //internal to implementation
+  void *helper;
 };
