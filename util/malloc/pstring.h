66e3d9784e5bc8e7ecde95e99aabc8603ca64c75
Goran Dakov <gorandakov@heptane.co.uk>
2024-06-16 16:21:05 +0100

    added some stuffe to c++ code.

--- util/malloc/pstring.h
+++ util/malloc/pstring.h
@@ -3,22 +3,27 @@ class pstring_hidden {
 public:
   int size;
   int capacity;
-  void *element;
+  char element[];
 };
 
-template <class T=char, struct __HEAP *h> class pstring : pstring_hidden {
-  pstring() {
-      size=0;
-      capacity=0;
-      element=0;
-  }
+template <class T=char, struct __HEAP *h> class pstring {
+  char *valp;
+  unsigned long peekchars;
+public:
   pstring (long _size) {
-    size=0;
-    capacity=_size;
-    element=h.malloc((size+1)*sizeof T);
-    if (!element) capacity=0;
+    valp=h.malloc((_size+1)+8);
+    if (!valp) __throw_out_of_memory();
+    valp+=8;
   }
   ~pstring() {
-    if (element) free(element);
+    if (valp) free(valp-8);
+  }
+  void resize(long _size) {
+    valp-=8;
+    h.realloc(valp,_size+1+8);
+  }
+  void post_update(void) {
+    peekchars=*(unsigned long *) valp;
+    __asm("bswapq %1,%0\n",peekchars : "+r",peekchars : "r");
   }
 };
