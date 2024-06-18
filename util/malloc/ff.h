800664be6f0578208dc60a384268bf296db998cf
Goran Dakov <gorandakov@heptane.co.uk>
2024-06-18 19:34:00 +0100

    added some stuffe.

--- util/malloc/ff.h
+++ util/malloc/ff.h
@@ -17,10 +17,13 @@ template <TObject.*void field, class T> class GC_PTR {
       return *valp;
   }
   TObject *get_object() {
-      TObject *obj=(TObject *) this;
-      unsigned long f=(unsigned long) field;
-      TObject.*void field2=(TObject.*void) -f;
-      return (TObject *) obj->field2;
+    unsigned long valp2=(unsigned long) valp;
+    unsigned long mask=0xffffffff800;
+    unsigned int exp=(valp2>>44)&0x1f;
+    valp2&=mask<<exp;
+    unsigned int low=(valp2>>52)&0xff0;
+    valp2|=low<<exp;
+    return valp2;
   }
   GC_PTR<field,T>(GC_PTR<field,T> &from) {
     __ptr_move(from.get_object,from,get_object,*this);
